9c62c2e7074a4d5d532389faff065f79ea055ae3
Magnus Christian Larsen <happymansi@hotmail.com>
2022-02-16 22:41:42 +0100

    Added a testing class for usage with the Stemmer class

---
+++ src/test/java/dk/sdu/se_f22/bim4/StemmerTest.java
@@ -0,0 +1,13 @@
+package dk.sdu.se_f22.bim4;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class StemmerTest {
+
+    private Stemmer stemmer;
+
+    @BeforeEach
+    private void initStemmer() {
+        stemmer = new Stemmer();
+    }
+}

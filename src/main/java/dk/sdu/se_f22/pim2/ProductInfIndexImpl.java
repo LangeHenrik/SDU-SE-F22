8998735512e61c057a277750648e3b708fc6bda4
EFCO <emilfco@icloud.com>
2022-02-22 09:06:52 +0100

    Added ProductInfIndexImpl & ProductInfSearchImpl

---
+++ src/main/java/dk/sdu/se_f22/pim2/ProductInfIndexImpl.java
@@ -0,0 +1,10 @@
+package dk.sdu.se_f22.pim2;
+
+import java.util.List;
+
+public class ProductInfIndexImpl implements  ProductInfIndex{
+    @Override
+    public void indexProducts(List<Product> products) {
+        //Blank
+    }
+}
---
+++ src/main/java/dk/sdu/se_f22/pim2/ProductInfSearchImpl.java
@@ -0,0 +1,11 @@
+package dk.sdu.se_f22.pim2;
+
+import java.util.List;
+
+public class ProductInfSearchImpl implements ProductInfSearch {
+
+    @Override
+    public List<Product> searchProducts(List<String> tokens) {
+        return null;
+    }
+}

29b7b0fc72072cdb76e1546c39536d15d564cf11
Magnus Jensen <magnushjensen.mail@gmail.com>
2022-02-22 08:35:08 +0100

    forgot to save

--- src/main/java/dk/sdu/se_f22/pim2/ProductIndexInfrastructure.java
+++ src/main/java/dk/sdu/se_f22/pim2/ProductIndexInfrastructure.java
@@ -1,46 +1,5 @@
 package dk.sdu.se_f22.pim2;
 
-import dk.sdu.se_f22.pim2.data.TokenParameter;
-import dk.sdu.se_f22.pim2.domain.ProductInfIndex;
-import dk.sdu.se_f22.pim2.domain.ProductInfIndexImpl;
-import dk.sdu.se_f22.pim2.domain.ProductInfSearch;
-import dk.sdu.se_f22.pim2.domain.ProductInfSearchImpl;
-
-import java.util.List;
-
 public class ProductIndexInfrastructure {
-	private static ProductIndexInfrastructure INSTANCE;
-	private final TokenParameter tokenParameter;
-
-	private ProductIndexInfrastructure() {
-		tokenParameter = TokenParameter.load();
-	}
-
-	public ProductInfSearch getProductSearch() {
-		return new ProductInfSearchImpl();
-	}
-
-	public ProductInfIndex getProductIndex() {
-		return new ProductInfIndexImpl();
-	}
-
-	public void setTokenParameters(String delimiter, List<String> ignoredChars) {
-		this.tokenParameter.setDelimiter(delimiter);
-		this.tokenParameter.setIgnoredChars(ignoredChars);
-		this.tokenParameter.save();
-	}
-
-	public TokenParameter getTokenParameter() {
-		return this.tokenParameter;
-	}
-
-	public static ProductIndexInfrastructure getInstance() {
-		if (INSTANCE == null) {
-			INSTANCE = new ProductIndexInfrastructure();
-		}
-
-		return INSTANCE;
-	}
-
 
 }

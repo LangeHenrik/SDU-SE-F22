41bcaf40f9e04047892c6d413427322844144ce1
Magnus Jensen <magnushjensen.mail@gmail.com>
2022-02-22 09:22:41 +0100

    added outcommented search method impl

--- src/main/java/dk/sdu/se_f22/pim2/ProductInfSearchImpl.java
+++ src/main/java/dk/sdu/se_f22/pim2/ProductInfSearchImpl.java
@@ -6,6 +6,7 @@ public class ProductInfSearchImpl implements ProductInfSearch {
 
     @Override
     public List<Product> searchProducts(List<String> tokens) {
+        // return PIM3.searchProducts(tokens);
         return null;
     }
 }

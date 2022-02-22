cf74e73362c34ff105924d86bc0801c5c60224a1
ZurenoC <92368939+ZurenoC@users.noreply.github.com>
2022-02-22 09:00:08 +0100

    continue

---
+++ src/main/java/dk/sdu/se_f22/pim2/ProductInfIndex.java
@@ -0,0 +1,14 @@
+package dk.sdu.se_f22.pim2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface ProductInfIndex {
+
+void indexProducts(List<Product> products);
+
+
+
+
+
+}
---
+++ src/main/java/dk/sdu/se_f22/pim2/ProductInfSearch.java
@@ -0,0 +1,12 @@
+package dk.sdu.se_f22.pim2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface ProductInfSearch {
+
+
+    List<Product> searchProducts(List<String> tokens);
+
+
+}

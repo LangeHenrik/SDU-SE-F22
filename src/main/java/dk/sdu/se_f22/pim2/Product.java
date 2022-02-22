096a3538a62a154d4f74f510273f8176130078f9
Magnus Jensen <magnushjensen.mail@gmail.com>
2022-02-22 11:18:30 +0100

    added data extraction

--- src/main/java/dk/sdu/se_f22/pim2/Product.java
+++ src/main/java/dk/sdu/se_f22/pim2/Product.java
@@ -1,4 +1,5 @@
 package dk.sdu.se_f22.pim2;
 
 public class Product {
+	public String description = "My awsome Product is boring!";
 }
--- src/main/java/dk/sdu/se_f22/pim2/domain/ProductInfIndexImpl.java
+++ src/main/java/dk/sdu/se_f22/pim2/domain/ProductInfIndexImpl.java
@@ -4,6 +4,8 @@ import dk.sdu.se_f22.pim2.Product;
 import dk.sdu.se_f22.pim2.ProductIndexInfrastructure;
 import dk.sdu.se_f22.pim2.data.TokenParameter;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ProductInfIndexImpl implements  ProductInfIndex{
@@ -12,18 +14,18 @@ public class ProductInfIndexImpl implements  ProductInfIndex{
         // Blank
     }
 
-    private List<String> tokenFilter (List<String> tokens){
+    private List<String> tokenFilter(List<String> tokens){
         //tokens = BIM4.filter(tokens);
         //tokens = CMS.filter(tokens);
         //tokens = PIM4.filter(tokens);
         return tokens;
     }
 
-    private List<String> tokenize (Product product){
-        List<String> tokens; //Data extraction
-        TokenParameter ignoredChars1 = ProductIndexInfrastructure.getInstance().getTokenParameter();
+    private List<String> tokenize(Product product){
+        TokenParameter tokenParameter = ProductIndexInfrastructure.getInstance().getTokenParameter();
+        List<String> tokens = this.extractData(product, tokenParameter.getDelimiter());
         for (String token : tokens){
-             for (String ignoredChar : ignoredChars1.getIgnoredChars()){
+             for (String ignoredChar : tokenParameter.getIgnoredChars()){
                  if(token.contains(ignoredChar)){
                      token = token.replaceAll(ignoredChar, "");
                  }
@@ -31,5 +33,11 @@ public class ProductInfIndexImpl implements  ProductInfIndex{
              }
         }
         return tokens;
+    }
+
+    private List<String> extractData(Product product, String delimiter) {
+        return new ArrayList<>(
+            Arrays.asList(product.description.split(delimiter))
+        );
     }
 }

e2cc7cdf9680b42d5da086f191f826c1c9e15aac
Kim JE Lassen <kimjelassen@yahoo.com>
2022-02-22 09:19:27 +0100

    Added ProductIndexInfrastructure

--- src/main/java/dk/sdu/se_f22/pim2/ProductIndexInfrastructure.java
+++ src/main/java/dk/sdu/se_f22/pim2/ProductIndexInfrastructure.java
@@ -1,5 +1,21 @@
 package dk.sdu.se_f22.pim2;
 
 public class ProductIndexInfrastructure {
+    private ProductIndexInfrastructure() {
+    }
 
+    private static final ProductIndexInfrastructure INSTANCE = new ProductIndexInfrastructure();
+
+    public ProductInfSearch getProductSearch() {
+        return new ProductInfSearchImpl();
+    }
+
+    public ProductInfIndex getProductIndex() {
+        return new ProductInfIndexImpl();
+    }
+
+    public static ProductIndexInfrastructure getInstance()
+    {
+        return INSTANCE;
+    }
 }

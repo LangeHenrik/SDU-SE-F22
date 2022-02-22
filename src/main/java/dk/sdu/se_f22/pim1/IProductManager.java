00b515954ffd226951a14019069eb5c8fc7eab71
Marcus Schebye Skrvier <marcus@familienskriver.dk>
2022-02-22 15:00:14 +0100

    Tilføjet interface og andre filer som andre hold skal bruge
    Kun product er funktionel i dette commit, en fungerende product manager kommer senere

---
+++ src/main/java/dk/sdu/se_f22/pim1/IProductManager.java
@@ -0,0 +1,19 @@
+package dk.sdu.se_f22.pim1;
+
+import java.util.ArrayList;
+
+public interface IProductManager {
+
+    boolean create(Product p);
+    boolean createAll(ArrayList<Product> p);
+    Product read(String productId);
+    Product[] readAll(String[] productIds);
+    boolean update(String productId, ProductAttribute a, String s);
+    boolean update(String productId, Product p);
+    boolean remove(String productId);
+    void setUpdateInterval(int time);
+    void reparse();
+
+    ArrayList<Product> getAllProducts();
+
+}

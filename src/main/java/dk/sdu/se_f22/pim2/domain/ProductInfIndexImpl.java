b2a9a1b5977e51b9c04d697fddf21ab0184ac23b
ZurenoC <92368939+ZurenoC@users.noreply.github.com>
2022-02-22 11:08:18 +0100

    tokenize metode

--- src/main/java/dk/sdu/se_f22/pim2/domain/ProductInfIndexImpl.java
+++ src/main/java/dk/sdu/se_f22/pim2/domain/ProductInfIndexImpl.java
@@ -1,6 +1,8 @@
 package dk.sdu.se_f22.pim2.domain;
 
 import dk.sdu.se_f22.pim2.Product;
+import dk.sdu.se_f22.pim2.ProductIndexInfrastructure;
+import dk.sdu.se_f22.pim2.data.TokenParameter;
 
 import java.util.List;
 
@@ -17,4 +19,17 @@ public class ProductInfIndexImpl implements  ProductInfIndex{
         return tokens;
     }
 
+    private List<String> tokenize (Product product){
+        List<String> tokens; //Data extraction
+        TokenParameter ignoredChars1 = ProductIndexInfrastructure.getInstance().getTokenParameter();
+        for (String token : tokens){
+             for (String ignoredChar : ignoredChars1.getIgnoredChars()){
+                 if(token.contains(ignoredChar)){
+                     token = token.replaceAll(ignoredChar, "");
+                 }
+
+             }
+        }
+        return tokens;
+    }
 }

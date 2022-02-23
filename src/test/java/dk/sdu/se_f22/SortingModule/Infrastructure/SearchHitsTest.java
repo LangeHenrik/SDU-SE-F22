32aabcd81fa81f9f3d3ff0de4078c98a113d9b76
Mikkel Albrechtsen <mikkel.alb@gmail.com>
2022-02-23 14:27:45 +0100

    Moved to nested setup

--- src/test/java/dk/sdu/se_f22/SortingModule/Infrastructure/SearchHitsTest.java
+++ src/test/java/dk/sdu/se_f22/SortingModule/Infrastructure/SearchHitsTest.java
@@ -3,6 +3,7 @@ package dk.sdu.se_f22.SortingModule.Infrastructure;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -42,126 +43,142 @@ class SearchHitsTest {
         assertEquals(this.sh, SearchHits.getInstance());
     }
 
-    @DisplayName("Set empty products test")
-    @Test
-    void setEmptyProducts() {
-        SearchHits.getInstance().setProducts(new ArrayList<>());
-        assertEquals(new ArrayList<>(), SearchHits.getInstance().getProducts());
+    @DisplayName("Set empty collection")
+    @Nested
+    public class SetEmptySearchHitsTest {
+        @DisplayName("Set empty products test")
+        @Test
+        void setEmptyProducts() {
+            SearchHits.getInstance().setProducts(new ArrayList<>());
+            assertEquals(new ArrayList<>(), SearchHits.getInstance().getProducts());
+        }
+
+        @DisplayName("Set empty brands test")
+        @Test
+        void setEmptyBrands() {
+            SearchHits.getInstance().setBrands(brands);
+            assertEquals(new ArrayList<>(), SearchHits.getInstance().getBrands());
+        }
+
+        @DisplayName("Set empty content test")
+        @Test
+        void setEmptyContents() {
+            SearchHits.getInstance().setContents(contents);
+            assertEquals(new ArrayList<>(), SearchHits.getInstance().getContents());
+        }
     }
 
-    @DisplayName("Set empty brands test")
-    @Test
-    void setEmptyBrands() {
-        SearchHits.getInstance().setBrands(brands);
-        assertEquals(new ArrayList<>(), SearchHits.getInstance().getBrands());
+    @DisplayName("Get empty collection")
+    @Nested
+    public class GetEmptySearchHitsTest {
+        @DisplayName("Get empty products test")
+        @Test
+        void getEmptyProducts() {
+            assertEquals(new ArrayList<>(), SearchHits.getInstance().getProducts());
+        }
+
+        @DisplayName("Get empty brands test")
+        @Test
+        void getEmptyBrands() {
+            assertEquals(new ArrayList<>(), SearchHits.getInstance().getBrands());
+        }
+
+        @DisplayName("Get empty content test")
+        @Test
+        void getEmptyContents() {
+            assertEquals(new ArrayList<>(), SearchHits.getInstance().getContents());
+        }
     }
 
-    @DisplayName("Set empty content test")
-    @Test
-    void setEmptyContents() {
-        SearchHits.getInstance().setContents(contents);
-        assertEquals(new ArrayList<>(), SearchHits.getInstance().getContents());
+    @DisplayName("Add elements through get methods")
+    @Nested
+    public class AddGetSearchHitsTest {
+        @DisplayName("Add products through getProducts")
+        @Test
+        void addProductsGet() {
+            SearchHits hits = SearchHits.getInstance();
+            Collection productsHits = hits.getProducts();
+
+            productsHits.add("Product 1");
+            productsHits.add("Product 2");
+            productsHits.add("Product 3");
+
+            assertEquals(productsHits, SearchHits.getInstance().getProducts());
+        }
+
+        @DisplayName("Add brands through getBrands")
+        @Test
+        void addBrandsGet() {
+            SearchHits hits = SearchHits.getInstance();
+            Collection brandHits = hits.getBrands();
+
+            brandHits.add("Brand 1");
+            brandHits.add("Brand 2");
+            brandHits.add("Brand 3");
+
+            assertEquals(brandHits, SearchHits.getInstance().getBrands());
+        }
+
+        @DisplayName("Add contents through getContents")
+        @Test
+        void addContentsGet() {
+            SearchHits hits = SearchHits.getInstance();
+            Collection contentsHits = hits.getContents();
+
+            contentsHits.add("Page 1");
+            contentsHits.add("Page 2");
+            contentsHits.add("Page 3");
+
+            assertEquals(contentsHits, SearchHits.getInstance().getContents());
+        }
     }
 
-    @DisplayName("Get empty products test")
-    @Test
-    void getEmptyProducts() {
-        assertEquals(new ArrayList<>(), SearchHits.getInstance().getProducts());
-    }
+    @DisplayName("Add elements through set methods")
+    @Nested
+    public class AddSetSearchHitsTest {
+        @DisplayName("Add products through setProducts")
+        @Test
+        void addProductsSet() {
+            SearchHits hits = SearchHits.getInstance();
+            Collection productsHits = new ArrayList<>();
 
-    @DisplayName("Get empty brands test")
-    @Test
-    void getEmptyBrands() {
-        assertEquals(new ArrayList<>(), SearchHits.getInstance().getBrands());
-    }
+            productsHits.add("Product 1");
+            productsHits.add("Product 2");
+            productsHits.add("Product 3");
 
-    @DisplayName("Get empty content test")
-    @Test
-    void getEmptyContents() {
-        assertEquals(new ArrayList<>(), SearchHits.getInstance().getContents());
-    }
+            hits.setProducts(productsHits);
 
-    @DisplayName("Add products through getProducts")
-    @Test
-    void addProductsGet() {
-        SearchHits hits = SearchHits.getInstance();
-        Collection productsHits = hits.getProducts();
+            assertEquals(productsHits, SearchHits.getInstance().getProducts());
+        }
 
-        productsHits.add("Product 1");
-        productsHits.add("Product 2");
-        productsHits.add("Product 3");
+        @DisplayName("Add brands through setBrands")
+        @Test
+        void addBrandsSet() {
+            SearchHits hits = SearchHits.getInstance();
+            Collection brandHits = new ArrayList<>();
 
-        assertEquals(productsHits, SearchHits.getInstance().getProducts());
-    }
+            brandHits.add("Brand 1");
+            brandHits.add("Brand 2");
+            brandHits.add("Brand 3");
 
-    @DisplayName("Add brands through getBrands")
-    @Test
-    void addBrandsGet() {
-        SearchHits hits = SearchHits.getInstance();
-        Collection brandHits = hits.getBrands();
+            hits.setBrands(brandHits);
 
-        brandHits.add("Brand 1");
-        brandHits.add("Brand 2");
-        brandHits.add("Brand 3");
+            assertEquals(brandHits, SearchHits.getInstance().getBrands());
+        }
 
-        assertEquals(brandHits, SearchHits.getInstance().getBrands());
-    }
+        @DisplayName("Add contents through setContents")
+        @Test
+        void addContentsSet() {
+            SearchHits hits = SearchHits.getInstance();
+            Collection contentsHits = new ArrayList<>();
 
-    @DisplayName("Add contents through getContents")
-    @Test
-    void addContentsGet() {
-        SearchHits hits = SearchHits.getInstance();
-        Collection contentsHits = hits.getContents();
+            contentsHits.add("Page 1");
+            contentsHits.add("Page 2");
+            contentsHits.add("Page 3");
 
-        contentsHits.add("Page 1");
-        contentsHits.add("Page 2");
-        contentsHits.add("Page 3");
+            hits.setContents(contentsHits);
 
-        assertEquals(contentsHits, SearchHits.getInstance().getContents());
-    }
-
-    @DisplayName("Add products through setProducts")
-    @Test
-    void addProductsSet() {
-        SearchHits hits = SearchHits.getInstance();
-        Collection productsHits = new ArrayList<>();
-
-        productsHits.add("Product 1");
-        productsHits.add("Product 2");
-        productsHits.add("Product 3");
-
-        hits.setProducts(productsHits);
-
-        assertEquals(productsHits, SearchHits.getInstance().getProducts());
-    }
-
-    @DisplayName("Add brands through setBrands")
-    @Test
-    void addBrandsSet() {
-        SearchHits hits = SearchHits.getInstance();
-        Collection brandHits = new ArrayList<>();
-
-        brandHits.add("Brand 1");
-        brandHits.add("Brand 2");
-        brandHits.add("Brand 3");
-
-        hits.setBrands(brandHits);
-
-        assertEquals(brandHits, SearchHits.getInstance().getBrands());
-    }
-
-    @DisplayName("Add contents through setContents")
-    @Test
-    void addContentsSet() {
-        SearchHits hits = SearchHits.getInstance();
-        Collection contentsHits = new ArrayList<>();
-
-        contentsHits.add("Page 1");
-        contentsHits.add("Page 2");
-        contentsHits.add("Page 3");
-
-        hits.setContents(contentsHits);
-
-        assertEquals(contentsHits, SearchHits.getInstance().getContents());
+            assertEquals(contentsHits, SearchHits.getInstance().getContents());
+        }
     }
 }

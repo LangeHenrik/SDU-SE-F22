e3a18c6ada194c3b396d3692789301b33104b524
Haim-hub <niklas@haim.dk>
2022-03-01 14:31:52 +0100

    testclass created
    created testclass for brandinfrastructure

---
+++ src/test/java/dk/sdu/se_f22/brandmodule/infrastructure/BrandInfrastructureTest.java
@@ -0,0 +1,29 @@
+package dk.sdu.se_f22.brandmodule.infrastructure;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BrandInfrastructureTest {
+    @Test
+    void loadTokenizationParameters() {
+    
+    }
+
+    @Test
+    void saveTokenizationParameters() {
+    }
+
+    @Test
+    void getBrandsFromSearchTokens() {
+    }
+
+    @Test
+    void indexBrands() {
+    }
+
+    @Test
+    void setTokenizationParameters() {
+    }
+}

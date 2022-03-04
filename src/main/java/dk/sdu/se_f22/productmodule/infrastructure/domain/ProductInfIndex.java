130c1e3642843ad38802720d50b6ea897ea9093c
Marcus Schebye Skrvier <marcus@familienskriver.dk>
2022-03-04 14:41:55 +0100

    Fixed library mismatch

--- src/main/java/dk/sdu/se_f22/productmodule/infrastructure/domain/ProductInfIndex.java
+++ src/main/java/dk/sdu/se_f22/productmodule/infrastructure/domain/ProductInfIndex.java
@@ -1,6 +1,6 @@
-package dk.sdu.se_f22.ProductModule.Infrastructure.domain;
+package dk.sdu.se_f22.productmodule.infrastructure.domain;
 
-import dk.sdu.se_f22.SharedLibrary.models.Product;
+import dk.sdu.se_f22.sharedlibrary.models.Product;
 
 import java.util.List;
 

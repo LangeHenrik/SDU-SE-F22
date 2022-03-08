5a76c4097045fdc54e700f7c96cd5c0a54025dc4
Sindahlof <frsin21@student.sdu.dk>
2022-03-08 10:13:49 +0100

    added test

--- src/test/java/dk/sdu/se_f22/searchmodule/twowaysynonyms/TestTwoWaySynonym.java
+++ src/test/java/dk/sdu/se_f22/searchmodule/twowaysynonyms/TestTwoWaySynonym.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 
+import java.util.ArrayList;
+
 public class TestTwoWaySynonym {
     static TwoWaySynonym operator = TwoWaySynonym.getInstance();
 
@@ -16,5 +18,25 @@ public class TestTwoWaySynonym {
     @Test
     @DisplayName("Update group id from an existing synonym")
     public void testUpdateGroupID(){
+    }
+
+    @Test
+    public void testFilter(){
+        ArrayList<String> tokends = new ArrayList<>();
+        ArrayList<String> expectedOutput = new ArrayList<>();
+
+        tokends.add("anger");
+        tokends.add("sad");
+
+        expectedOutput.add("anger");
+        expectedOutput.add("acrimony");
+        expectedOutput.add("annoyance");
+        expectedOutput.add("sad");
+        expectedOutput.add("bitter");
+        expectedOutput.add("heartbroken");
+
+        TwoWaySynonym.getInstance().filter(tokends);
+
+        Assertions.assertEquals(expectedOutput,tokends);
     }
 }

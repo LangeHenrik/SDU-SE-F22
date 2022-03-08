bda28236bd55c40a31c2b9811cb56f89581a30e3
Emre Eker <emeke21@student.sdu.dk>
2022-03-08 11:05:27 +0100

    implemented Item class

--- src/main/java/dk/sdu/se_f22/searchmodule/onewaysynonyms/Item.java
+++ src/main/java/dk/sdu/se_f22/searchmodule/onewaysynonyms/Item.java
@@ -1,4 +1,30 @@
 package dk.sdu.se_f22.searchmodule.onewaysynonyms;
 
+import java.util.LinkedList;
+
 public class Item {
+    //Attributes
+    private String name;
+    private Item superItem;
+    private LinkedList<Item> subItems;
+
+    //Constructor
+    public Item(String name){
+        this.name = name;
+    }
+    public Item(String name, Item superItem){
+        this(name);
+        this.superItem = superItem;
+    }
+
+    //Methods
+    public void AddSubItem(Item subItem){
+        subItems.add(subItem);
+    }
+    public void setSuperItem(Item superItem) {
+        this.superItem = superItem;
+    }
+    public Boolean removeSubItem(Item item){
+        return subItems.remove(item);
+    }
 }

9d50de0b38a7dee8a74d4702237f55da0ec8c4b9
frmad <frmad21@student.sdu.dk>
2022-03-08 11:31:04 +0100

    Add method

--- src/main/java/dk/sdu/se_f22/searchmodule/misspellings/Misspellings.java
+++ src/main/java/dk/sdu/se_f22/searchmodule/misspellings/Misspellings.java
@@ -1,11 +1,73 @@
 package dk.sdu.se_f22.searchmodule.misspellings;
 
+import java.sql.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Misspellings implements DatabaseOperator{
+    private String wrongSpelling;
+    private String correctSpelling;
+    private final String url = "jdbc:postgresql://abul.db.elephantsql.com/hzajyqbo";
+    private final String user = "hzajyqbo";
+    private final String password = "K8664qtGojuBvQczzv66EhaqkUNbXLj0";
+    private static final String INSERT_misspellings = "INSERT INTO misspellings (wrong, correct) VALUES (?,?);";
 
     @Override
     public ArrayList<String> filter(ArrayList<String> tokens) {
-        return null;
+        ArrayList<String> corrected = new ArrayList<String>();
+        String databaseMatch = "noget"; //temp
+        String databaseMatchCorrected = "nogetandet"; //temp
+
+        for (String s: tokens){
+            if (s == databaseMatch) {
+                s = databaseMatchCorrected;
+            }
+            corrected.add(s);
+        }
+        return corrected;
     }
+
+    /*
+    Help found at:
+    https://www.javaguides.net/2020/02/java-jdbc-postgresql-insert-example.html
+     */
+    public void addMispellings() throws SQLException {
+        //get the new misspelling
+        Scanner scannerW = new Scanner(System.in);
+        System.out.println("Write misspelling");
+        String misspelling = scannerW.nextLine();
+        //get the correct word
+        Scanner scannerC = new Scanner(System.in);
+        System.out.println("Write the correct spelling of the word");
+        String correct = scannerC.nextLine();
+        //add the new misspelling
+        // Step 1: Establishing a Connection
+        try (Connection connection = DriverManager.getConnection(url, user, password);
+             // Step 2:Create a statement using connection object
+             PreparedStatement preparedStatement = connection.prepareStatement(INSERT_misspellings)) {
+                preparedStatement.setString(1, misspelling);
+                preparedStatement.setString(2, correct);
+
+            System.out.println(preparedStatement);
+            //System.out.println("The misspelling: "+misspelling+" have been added."+" The correct spelling is "+correct);
+            // Step 3: Execute the query or update query
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Could not add misspelling maybe it already exists");
+        }
+    }
+
+    public void deleteMispellings(String word){
+
+    }
+
+    public void updateMispellings(String word){
+
+    }
+
+    public static void main(String[] args) throws SQLException {
+        Misspellings mis = new Misspellings();
+        mis.addMispellings();
+    }
+
 }
---
+++ src/test/java/dk/sdu/se_f22/searchmodule/misspellings/MisspellingsTest.java
@@ -0,0 +1,41 @@
+package dk.sdu.se_f22.searchmodule.misspellings;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MisspellingsTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void filter() {
+
+    }
+
+    @Test
+    void addMisspellings() {
+
+    }
+
+    @Test
+    void deleteMisspellings() {
+
+    }
+
+    @Test
+    void updateMisspellings() {
+
+    }
+}

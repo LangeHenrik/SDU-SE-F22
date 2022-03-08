fd49f9ba493f377c40293f15070baca995136225
thoni21 <90039692+thoni21@users.noreply.github.com>
2022-03-08 10:48:52 +0100

    Update ReplaceForbiddenChars.java
    Added 0-9 to regex

--- src/main/java/dk/sdu/se_f22/searchmodule/infrastructure/ReplaceForbiddenChars.java
+++ src/main/java/dk/sdu/se_f22/searchmodule/infrastructure/ReplaceForbiddenChars.java
@@ -1,9 +1,9 @@
-package dk.sdu.se_f22.searchmodule.infrastructure.interfaces;
+package dk.sdu.se_f22.searchmodule.infrastructure;
 
 public class ReplaceForbiddenChars {
 
     public String removeForbiddenChars(String toSort){
-        toSort = toSort.replaceAll("[^a-zA-Z ]","");
+        toSort = toSort.replaceAll("[^0-9a-zA-Z ]","");
         return toSort;
     }
 

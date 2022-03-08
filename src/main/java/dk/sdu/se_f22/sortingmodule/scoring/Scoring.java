e839788f496c5dac02ea1df4d00b8deaff3595c1
Magnus Jensen <magnushjensen.mail@gmail.com>
2022-03-08 15:01:40 +0100

    comment out psuedo-code

--- src/main/java/dk/sdu/se_f22/sortingmodule/scoring/Scoring.java
+++ src/main/java/dk/sdu/se_f22/sortingmodule/scoring/Scoring.java
@@ -6,49 +6,49 @@ public class Scoring implements IScoring{
 
     private void price(List<Object> input) {
         for (Object o : input) {
-            int price = o.getPrice();
+           /* int price = o.getPrice();
 
             for (int j = 0; j < db.size(); j++) {
                 if (price < db(j).get(Price.bracket)) {
                     o.setScore(o.getScore() + db(j).get(Price.weight));
                 }
-            }
+            }*/
         }
     }
 
     private void review(List<Object> input) {
         for (Object o : input) {
-            int review = o.getReview();
+           /* int review = o.getReview();
 
             for (int j = 0; j < db.size(); j++) {
                 if (review < db(j).get(Review.bracket)) {
                     o.setScore(o.getScore() + db(j).get(Review.weight));
                 }
-            }
+            }*/
         }
     }
 
     private void stock(List<Object> input) {
         for (Object o : input) {
-            int stock = o.getStock();
+            /*int stock = o.getStock();
 
             for (int j = 0; j < db.size(); j++) {
                 if (stock < db(j).get(Stock.bracket)) {
                     o.setScore(o.getScore() + db(j).getStock.weight));
                 }
-            }
+            }*/
         }
     }
 
     private void releaseDate(List<Object> input) {
         for (Object o : input) {
-            int releaseDate = o.getReleaseDate();
+            /*int releaseDate = o.getReleaseDate();
 
             for (int j = 0; j < db.size(); j++) {
                 if (releaseDate < db(j).get(ReleaseDate.bracket)) {
                     o.setScore(o.getScore() + db(j).get(ReleaseDate.weight));
                 }
-            }
+            }*/
         }
     }
 

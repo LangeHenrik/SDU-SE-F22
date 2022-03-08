745f99ad38a2263cf3b4e9a0c2b059f99c99fd13
v-nemeth <viand21@student.sdu.dk>
2022-03-08 08:22:48 +0100

    BIM-1_Public interface

---
+++ src/main/java/dk/sdu/se_f22/brandmodule/management/Bim.java
@@ -0,0 +1,35 @@
+package dk.sdu.se_f22.brandmodule.management;
+import dk.sdu.se_f22.sharedlibrary.models.Brand;
+
+public class Bim implements IBim {
+
+    @Override
+    public void createBrand(Brand brand) {
+
+    }
+
+    @Override
+    public Brand getBrand(int id) {
+        return null;
+    }
+
+    @Override
+    public Brand getBrand(String name) {
+        return null;
+    }
+
+    @Override
+    public void removeBrand(Brand brand) {
+
+    }
+
+    @Override
+    public Brand updateBrand(Brand brand) {
+        return null;
+    }
+
+    @Override
+    public void setIndexInterval(int indexInterval) {
+
+    }
+}

60983dd7b5d94777c0553a2033f7af5443f865df
Daniel <68541911+Vildtorben@users.noreply.github.com>
2022-03-08 12:26:26 +0100

    Update SQL-create-statement.sql

--- src/main/resources/dk/sdu/se_f22/sortingmodule/infrastructure/SQL-create-statement.sql
+++ src/main/resources/dk/sdu/se_f22/sortingmodule/infrastructure/SQL-create-statement.sql
@@ -1,22 +1,22 @@
 
-CREATE TABLE querys (
+CREATE TABLE queries (
 	id SERIAL PRIMARY KEY,
-	tekst VARCHAR(512) NOT NULL,
+	text VARCHAR(512) NOT NULL,
 	date_stamp TIMESTAMP NOT NULL
 
 );
 
 
-CREATE TABLE querys_settings (
-	querys_id INT REFERENCES querys(id),
+CREATE TABLE queries_settings (
+	queries_id INT REFERENCES queries(id),
 	key VARCHAR(255) NOT NULL,
 	value VARCHAR(255) NOT NULL,
-	PRIMARY KEY (querys_id, key)
+	PRIMARY KEY (queries_id, key)
 
 );
 
-CREATE TABLE querys_category(
-	querys_id INT REFERENCES querys(id),
+CREATE TABLE queries_category(
+	queries_id INT REFERENCES queries(id),
 	catergory_id INT NOT NULL,
-	PRIMARY KEY (querys_id, catergory_id)
+	PRIMARY KEY (queries_id, catergory_id)
 );

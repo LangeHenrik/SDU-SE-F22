e80742f53495264eae02c07b8c0d62645f2064e1
frmad <frmad21@student.sdu.dk>
2022-03-08 11:31:04 +0100

    Add method

---
+++ src/test/java/dk/sdu/se_f22/searchmodule/misspellings/TestSuite.java
@@ -0,0 +1,9 @@
+package dk.sdu.se_f22.searchmodule.misspellings;
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses({MisspellingsTest.class})
+public class TestSuite {
+}

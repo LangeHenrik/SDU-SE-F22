ba14def5470a7e090a92b9d7ad7772a183a8b214
Sindahlof <frsin21@student.sdu.dk>
2022-03-08 10:27:40 +0100

    added sorting module's own mess

--- src/test/java/dk/sdu/se_f22/searchmodule/twowaysynonyms/TestTwoWaySynonym.java
+++ src/test/java/dk/sdu/se_f22/searchmodule/twowaysynonyms/TestTwoWaySynonym.java
@@ -100,7 +100,6 @@ public class TestTwoWaySynonym {
     @DisplayName("Update group id from an existing synonym")
     public void testUpdateGroupID(){
     }
-
     @Test
     public void testFilter(){
         ArrayList<String> tokends = new ArrayList<>();

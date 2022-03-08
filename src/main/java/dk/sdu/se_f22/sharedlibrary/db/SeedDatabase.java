c686c20b721beae8082b1b114ff11538d42a0663
v-nemeth <viand21@student.sdu.dk>
2022-03-08 11:29:07 +0100

    main-metode i SeedDatabase

--- src/main/java/dk/sdu/se_f22/sharedlibrary/db/SeedDatabase.java
+++ src/main/java/dk/sdu/se_f22/sharedlibrary/db/SeedDatabase.java
@@ -1,7 +1,18 @@
 package dk.sdu.se_f22.sharedlibrary.db;
 
+
+import dk.sdu.se_f22.brandmodule.management.persistence.Persistence;
+
 public class SeedDatabase {
     //Sørger for at kalde hver gruppes seedDatabase metode:
 
+    public static void main(String[] args) {
+        System.out.println("Not yet implemented");
 
+
+        //----------------Seeding af brands----------------------
+        //Persistence brandPersistence = new Persistence();
+        //brandPersistence.seedDatabase();
+
+    }
 }

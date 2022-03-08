f02e95104edd94783e2177a4f8bdf7c0fc844149
Emre Eker <emeke21@student.sdu.dk>
2022-03-08 12:26:27 +0100

    Implemented ItemCatalog

--- src/main/java/dk/sdu/se_f22/searchmodule/onewaysynonyms/ItemCatalog.java
+++ src/main/java/dk/sdu/se_f22/searchmodule/onewaysynonyms/ItemCatalog.java
@@ -1,4 +1,38 @@
 package dk.sdu.se_f22.searchmodule.onewaysynonyms;
 
+import java.util.LinkedList;
+
 public class ItemCatalog {
+    //Attributes
+    private LinkedList<Item> catalog;
+
+    //Constructor
+    public ItemCatalog(){
+        this.catalog = new LinkedList<>();
+        addSubItems();
+    }
+
+    //Methods
+    public void addITem(Item item){
+        catalog.add(item);
+    }
+
+    public boolean removeItem(Item item){
+        return catalog.remove(item);
+    }
+
+    private void addSubItems(){
+        for(Item item : catalog){
+            item.getSuperItem().AddSubItem(item);
+        }
+    }
+
+    public LinkedList<Item> oneWaySynonymStrings(String string) throws notFoundException {
+        for(Item item : catalog) {
+            if(item.getName().equals(string)){
+                return item.getSubItems();
+            }
+        }
+        throw new notFoundException("Item not found in database.");
+    }
 }

c4091a78afd196d6c3d0178f6640c2af92ba234c
Mikkel Albrechtsen <mikkel.alb@gmail.com>
2022-03-08 10:44:25 +0100

    Generated SearchQuery class structure

--- src/main/java/dk/sdu/se_f22/sortingmodule/infrastructure/domain/SearchQuery.java
+++ src/main/java/dk/sdu/se_f22/sortingmodule/infrastructure/domain/SearchQuery.java
@@ -1,5 +1,46 @@
 package dk.sdu.se_f22.sortingmodule.infrastructure.domain;
 
-public class SearchQuery {
+import java.util.ArrayList;
+import java.util.HashMap;
 
+public class SearchQuery {
+    String query;
+    int[] pagination;
+    HashMap<Integer, String[]> range;
+    ArrayList<Integer> category;
+
+    public void setCategory(ArrayList<Integer> categories) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void addCategory(int category) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void clearCategory() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void addRange(int rangeId, String startRange, String endRange) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void clearRange() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void setPagination(int page, int pageSize) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void setScoring(String scoring) {
+        // TODO Auto-generated method stub
+        
+    }
 }

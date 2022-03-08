5b394e988938e5446e3fa07cb02944a95e6d4342
Patrick <Patrickbjerre@outlook.dk>
2022-03-08 13:18:24 +0100

    Now with getters and setters for some reason

--- src/main/java/dk/sdu/se_f22/brandmodule/management/services/JsonService.java
+++ src/main/java/dk/sdu/se_f22/brandmodule/management/services/JsonService.java
@@ -30,13 +30,13 @@ public class JsonService implements IJsonService {
                 var name = keys.next();
                 JSONObject properties =  (JSONObject) jo.get(name);
 
-                Brand brand = new Brand();
+                Brand brand = new Brand(0,null,null,null,null,null);
 
                 // Set single valued properties
-                brand.name = (name.toString());
-                brand.description = (properties.get("description").toString());
-                brand.founded = (properties.get("founded").toString());
-                brand.headquarters = (properties.get("headquarters").toString());
+                brand.setName((name.toString()));
+                brand.setDescription((properties.get("description").toString()));
+                brand.setFounded((properties.get("founded").toString()));
+                brand.setHeadquarters( (properties.get("headquarters").toString()));
 
                 // Build product array
                 JSONArray objs = (JSONArray) properties.get("products");
@@ -46,7 +46,7 @@ public class JsonService implements IJsonService {
                 iterator.forEachRemaining(p -> products.add(p.toString()));
 
                 // Set brand property
-                brand.products = (products);
+                brand.setProducts( (products));
 
                 // Add brand to list of brands
                 brands.add(brand);

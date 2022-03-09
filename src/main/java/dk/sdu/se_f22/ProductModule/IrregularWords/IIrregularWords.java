eeb5deb89555f35d8b633a134fca371cfa87be50
GustavFrandsen <gufra21@student.sdu.dk>
2022-03-09 14:18:38 +0100

    maybe done interface

--- src/main/java/dk/sdu/se_f22/ProductModule/IrregularWords/IIrregularWords.java
+++ src/main/java/dk/sdu/se_f22/ProductModule/IrregularWords/IIrregularWords.java
@@ -2,4 +2,22 @@ package dk.sdu.se_f22.ProductModule.IrregularWords;
 
 public interface IIrregularWords {
 
-}
+        public void createIRWord(int ID, String Word);
+
+        public void deleteIRWord(int serialKey);
+
+        public void updateIRWord();
+
+        public void readIRWord();
+
+        public void getIRWord();
+
+        public void createColumnIR(String CName);
+
+        public void deleteColumnIR(String CName);
+
+        public void createBackup();
+
+        public void loadBackup();
+
+    }

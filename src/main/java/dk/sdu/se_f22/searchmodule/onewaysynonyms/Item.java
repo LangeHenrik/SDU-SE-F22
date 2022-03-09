222b64578ebf573b5a966309f7c88d2716d2ba3d
Eivan20 <eivan20@student.sdu.dk>
2022-03-09 12:44:26 +0100

    CRUD methods in for databaseAPI

--- src/main/java/dk/sdu/se_f22/searchmodule/onewaysynonyms/Item.java
+++ src/main/java/dk/sdu/se_f22/searchmodule/onewaysynonyms/Item.java
@@ -43,7 +43,7 @@ public class Item {
     public Item getSuperItem() {
         return superItem;
     }
-    
+
     public static void main(String[] args) {
         OneWayImplementation implementation = new OneWayImplementation();
         implementation.showCatalog();

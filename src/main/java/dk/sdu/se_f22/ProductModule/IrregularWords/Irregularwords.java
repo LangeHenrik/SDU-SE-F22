4beb11d6d07fc1ff3553adf6f2a82f5ff041f9d1
GustavFrandsen <gufra21@student.sdu.dk>
2022-03-14 15:55:58 +0100

    more not finished

---
+++ src/main/java/dk/sdu/se_f22/ProductModule/IrregularWords/Irregularwords.java
@@ -0,0 +1,50 @@
+package dk.sdu.se_f22.ProductModule.IrregularWords;
+
+public class Irregularwords implements IIrregularWords{
+
+    
+    @Override
+    public void createIRWord(int ID, String Word) {
+
+    }
+
+    @Override
+    public void deleteIRWord(int serialKey) {
+
+    }
+
+    @Override
+    public void updateIRWord() {
+
+    }
+
+    @Override
+    public void readIRWord() {
+
+    }
+
+    @Override
+    public void getIRWord() {
+
+    }
+
+    @Override
+    public void createColumnIR(String CName) {
+
+    }
+
+    @Override
+    public void deleteColumnIR(String CName) {
+
+    }
+
+    @Override
+    public void createBackup() {
+
+    }
+
+    @Override
+    public void loadBackup() {
+
+    }
+}

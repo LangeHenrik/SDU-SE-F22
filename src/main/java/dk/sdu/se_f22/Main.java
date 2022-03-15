618ccf69f721da5064a345d894e168d4096814b5
Queztex <queztex@gmail.com>
2022-03-15 13:37:36 +0100

    Changes by Stokn9x

--- src/main/java/dk/sdu/se_f22/Main.java
+++ src/main/java/dk/sdu/se_f22/Main.java
@@ -1,7 +1,7 @@
 package dk.sdu.se_f22;
 
 import dk.sdu.se_f22.sharedlibrary.db.DBConnection;
-import java.sql.Connection;
+//import java.sql.Connection;
 
 public class Main {
     public static void main(String[] args) {

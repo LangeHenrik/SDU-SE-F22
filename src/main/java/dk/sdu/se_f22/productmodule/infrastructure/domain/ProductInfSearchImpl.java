4c322235383acc2250adcfe472e173260b98424e
EFCO <emilfco@icloud.com>
2022-03-15 12:13:12 +0100

    Test class implemented
    Test class for ProductInfSearchImpl implemented

--- src/main/java/dk/sdu/se_f22/productmodule/infrastructure/domain/ProductInfSearchImpl.java
+++ src/main/java/dk/sdu/se_f22/productmodule/infrastructure/domain/ProductInfSearchImpl.java
@@ -2,6 +2,7 @@ package dk.sdu.se_f22.productmodule.infrastructure.domain;
 
 import dk.sdu.se_f22.sharedlibrary.models.Product;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProductInfSearchImpl implements ProductInfSearch {
@@ -9,6 +10,6 @@ public class ProductInfSearchImpl implements ProductInfSearch {
     @Override
     public List<Product> searchProducts(List<String> tokens) {
         // return PIM3.searchProducts(tokens);
-        return null;
+        return new ArrayList<>();
     }
 }

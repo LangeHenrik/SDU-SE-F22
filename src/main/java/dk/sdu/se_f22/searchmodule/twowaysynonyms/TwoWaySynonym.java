f61aaec3862a341106fd20db8cef1ff39b0166cd
Oliver Kirkegaard Svendsen <oliverksvendsen@gmail.com>
2022-03-15 11:00:24 +0100

    solved merge conflicts

--- src/main/java/dk/sdu/se_f22/searchmodule/twowaysynonyms/TwoWaySynonym.java
+++ src/main/java/dk/sdu/se_f22/searchmodule/twowaysynonyms/TwoWaySynonym.java
@@ -1,11 +1,13 @@
 package dk.sdu.se_f22.searchmodule.twowaysynonyms;
 
+
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.UUID;
 
 public class TwoWaySynonym implements DatabaseOperator {
-    private TwoWaySynonym(){};
+    private TwoWaySynonym() {}
+
     public static TwoWaySynonym getInstance() {
         return TwoWaySynonymHolder.INSTANCE;
     }
@@ -16,13 +18,14 @@ public class TwoWaySynonym implements DatabaseOperator {
 
     /**
      * Add a new synonym and create a new synonym group.
-     * @param synonym           Word to add
-     * @return                  UUID of created Synonym
+     *
+     * @param synonym Word to add
+     * @return UUID of created Synonym
      */
     @Override
     public UUID create(String synonym) {
         PreparedStatement statement;
-        ArrayList<Object> statementList = new ArrayList<>(){{
+        ArrayList<Object> statementList = new ArrayList<>() {{
             add(UUID.randomUUID());
             add(synonym);
         }};
@@ -37,9 +40,10 @@ public class TwoWaySynonym implements DatabaseOperator {
 
     /**
      * Add a new synonym to an existing synonym group.
-     * @param synonym       Word to add
-     * @param groupMember   Existing related Synonym
-     * @return              UUID of created Synonym
+     *
+     * @param synonym     Word to add
+     * @param groupMember Existing related Synonym
+     * @return UUID of created Synonym
      */
     @Override
     public UUID create(String synonym, String groupMember) {
@@ -48,8 +52,9 @@ public class TwoWaySynonym implements DatabaseOperator {
 
     /**
      * Retrieve the data from the database related to the given synonym
-     * @param synonym   Word to receive synonyms for
-     * @return          ResultSet of all matching synonyms
+     *
+     * @param synonym Word to receive synonyms for
+     * @return ResultSet of all matching synonyms
      */
     @Override
     public ResultSet read(String synonym) {
@@ -57,10 +62,12 @@ public class TwoWaySynonym implements DatabaseOperator {
     }
 
     /**
+     * <<<<<<< HEAD
      * Update a synonyms group ID
-     * @param synonym           Word to update
-     * @param relatedSynonym    Related Synonym
-     * @return                  UUID of updated Synonym
+     *
+     * @param synonym        Word to update
+     * @param relatedSynonym Related Synonym
+     * @return UUID of updated Synonym
      */
     @Override
     public UUID updateGroupID(String synonym, String relatedSynonym) {
@@ -69,63 +76,74 @@ public class TwoWaySynonym implements DatabaseOperator {
 
     /**
      * Update spelling of a synonym
+     *
      * @param synonym           Word to update
      * @param correctedSpelling Corrected spelling of Synonym
-     * @return                  UUID of updated Synonym
+     * @return UUID of updated Synonym
      */
     @Override
     public UUID updateSpelling(String synonym, String correctedSpelling) {
+        /**Update a synonym(mainly spelling correction)
+         * @param synonym Word to update
+         * @param correctedSynonym Corrected form of Synonym
+         * @return UUID of updated Synonym
+         * */
+
         return null;
     }
-
-    /**
-     * Delete a synonym from the database
-     * @param synonym           Word to delete
-     * @return                  Boolean value
-     */
-    @Override
-    public boolean delete(String synonym) {
-        return false;
-    }
-
-    /**
-     * Goes through a list of tokens, to find all
-     * their respective two-way synonyms.
-     * @param tokens    List of keywords to find synonyms for
-     * @return          List of found synonyms and tokens
-     */
-    @Override
-    public ArrayList<String> filter(ArrayList<String> tokens) {
-        return null;
-    }
-
-    private void prepareStatement (PreparedStatement statement, ArrayList<Object> parameters){
-        try {
-            for (int i = 1; i < parameters.size(); i++) {
-                statement.setObject(i, parameters.get(i-1));
-            }
-        } catch (SQLException throwables) {
-            System.out.println("Preparing statement failed");
-            throwables.printStackTrace();
-        }
-    }
-
-    private void executeStatement (PreparedStatement statement) {
-        try{
-            statement.execute();
-        } catch (SQLException throwables){
-            System.out.println("Couldn't execute statement");
-            throwables.printStackTrace();
-        }
-    }
-
-    private UUID getUUID (ResultSet resultSet){
-        try{
-            return (UUID)resultSet.getObject("UUID");
-        } catch (SQLException throwables) {
-            System.out.println("No UUID found");
-            throwables.printStackTrace();
+        @Override
+        public UUID update (String synonym, String correctedSynonym){
             return null;
         }
-    }
+
+        /**
+         * Delete a synonym from the database
+         * @param synonym           Word to delete
+         * @return Boolean value
+         */
+        @Override
+        public boolean delete (String synonym){
+            return false;
+        }
+
+        /**
+         * Goes through a list of tokens, to find all
+         * their respective two-way synonyms.
+         * @param tokens    List of keywords to find synonyms for
+         * @return List of found synonyms and tokens
+         */
+        @Override
+        public ArrayList<String> filter (ArrayList < String > tokens) {
+            return null;
+        }
+
+        private void prepareStatement (PreparedStatement statement, ArrayList < Object > parameters){
+            try {
+                for (int i = 1; i < parameters.size(); i++) {
+                    statement.setObject(i, parameters.get(i - 1));
+                }
+            } catch (SQLException throwables) {
+                System.out.println("Preparing statement failed");
+                throwables.printStackTrace();
+            }
+        }
+
+        private void executeStatement (PreparedStatement statement){
+            try {
+                statement.execute();
+            } catch (SQLException throwables) {
+                System.out.println("Couldn't execute statement");
+                throwables.printStackTrace();
+            }
+        }
+
+        private UUID getUUID (ResultSet resultSet){
+            try {
+                return (UUID) resultSet.getObject("UUID");
+            } catch (SQLException throwables) {
+                System.out.println("No UUID found");
+                throwables.printStackTrace();
+                return null;
+            }
+        }
 }

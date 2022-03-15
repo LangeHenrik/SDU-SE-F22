297fede9c06014f167a82d9e2b6d8be6ff9d04be
Oliver Kirkegaard Svendsen <oliverksvendsen@gmail.com>
2022-03-15 11:16:19 +0100

    solved merge conflicts

--- src/main/java/dk/sdu/se_f22/searchmodule/twowaysynonyms/TwoWaySynonym.java
+++ src/main/java/dk/sdu/se_f22/searchmodule/twowaysynonyms/TwoWaySynonym.java
@@ -27,8 +27,7 @@ public class TwoWaySynonym implements DatabaseOperator {
             add(UUID.randomUUID());
             add(synonym);
         }};
-
-        statement = Foo.prepareStatement("INSERT INTO BAR(UUID, name) VALUES (?, ?)");
+        statement = Foo.prepareStatement("INSERT INTO BAR(uuid, name) VALUES (?, ?)");
         prepareStatement(statement, statementList);
         executeStatement(statement);
 

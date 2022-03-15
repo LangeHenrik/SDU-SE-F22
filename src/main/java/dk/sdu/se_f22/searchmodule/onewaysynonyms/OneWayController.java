5913718795a2b4806a9f1d141a2895be25aa0833
Eivan20 <eivan20@student.sdu.dk>
2022-03-15 13:39:06 +0100

    Setup fxml

---
+++ src/main/java/dk/sdu/se_f22/searchmodule/onewaysynonyms/OneWayController.java
@@ -0,0 +1,10 @@
+package dk.sdu.se_f22.searchmodule.onewaysynonyms;
+
+import javafx.event.ActionEvent;
+
+public class OneWayController {
+
+    public void onHelloButtonClick(ActionEvent actionEvent) {
+
+    }
+}
---
+++ src/main/java/dk/sdu/se_f22/searchmodule/onewaysynonyms/OneWayGUI.java
@@ -0,0 +1,24 @@
+package dk.sdu.se_f22.searchmodule.onewaysynonyms;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class OneWayGUI extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(OneWayGUI.class.getResource("dk/sdu/se_f22/searchmodule/onewaysynonyms/OneWayGUI.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(),320,240);
+        primaryStage.setTitle("OneWayGUI");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}

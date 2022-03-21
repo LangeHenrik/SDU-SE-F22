3991f007ffbf3e04c6d6d0ac04f96d985c3400b9
Emre Eker <90039700+Dingobob21@users.noreply.github.com>
2022-03-21 15:54:17 +0100

    Dårligt forsøg på at rebase

--- src/main/java/dk/sdu/se_f22/searchmodule/onewaysynonyms/ItemCatalog.java
+++ src/main/java/dk/sdu/se_f22/searchmodule/onewaysynonyms/ItemCatalog.java
@@ -1,8 +1,6 @@
 package dk.sdu.se_f22.searchmodule.onewaysynonyms;
 
-
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class ItemCatalog {
     //Attributes
@@ -29,7 +27,6 @@ public class ItemCatalog {
             try{
                 item.getSuperItem().AddSubItem(item);
             } catch (NullPointerException ex){
-
             }
         }
     }
@@ -43,4 +40,58 @@ public class ItemCatalog {
         }
         throw new notFoundException("Item not found in database.");
     }
+
+    private HashMap<Integer, Integer> fillNumberInEachGeneration(Item root){
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        map.put(0,1);
+        LinkedList<Item> childrenInGeneration = new LinkedList<>();
+        childrenInGeneration.add(root);
+        LinkedList<Item> nextGeneration = new LinkedList<>();
+        int cycle = 1;
+        while (true){
+            for(Item item : childrenInGeneration){
+                try{
+                    nextGeneration.addAll(item.getSubItems());
+                }catch (NullPointerException ex){
+                }
+            }
+            if(nextGeneration.size() == 0){
+                break;
+            }
+            map.put(cycle,nextGeneration.size());
+            cycle++;
+            childrenInGeneration.clear();
+            childrenInGeneration.addAll(nextGeneration);
+            nextGeneration.clear();
+        }
+        return map;
+    }
+   
+    public void drawImage(Item item){
+        HashMap<Integer, Integer> numberInEachGeneration = fillNumberInEachGeneration(item);
+
+        int width = imageWidth(numberInEachGeneration.keySet());
+        int length = imageLength(numberInEachGeneration.values());
+
+
+    }
+
+    private int imageLength(Collection<Integer> collection){
+        int biggest = 0;
+        for(int i : collection){
+            if(i > biggest){
+                biggest = i;
+            }
+        }
+        return biggest * 110;
+    }
+    private int imageWidth(Set<Integer> set){
+        return set.size() * 150;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
 }

92364db118527e361ccf5174aefd176efefa1da5
OFHansen <ohans21@student.sdu.dk>
2022-03-22 09:55:05 +0100

    Added parameter input methods and updated interface

--- src/main/java/dk/sdu/se_f22/searchmodule/misspellings/DatabaseOperator.java
+++ src/main/java/dk/sdu/se_f22/searchmodule/misspellings/DatabaseOperator.java
@@ -4,10 +4,15 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public interface DatabaseOperator extends Filterable{
-
+    public void addMisspelling(); //console
+    public void addMisspelling(String wrong, String correct); //parameter input
+    public void deleteMisspelling(); //console
+    public void deleteMisspelling(String missspelling); //parameter input
+    public void updateMisspelling(); //console
+    public void updateMisspelling(String correction, String oldMisspelling); //parameter input
 }
 
 //Temporary
 interface Filterable {
-    ArrayList<String> filter(ArrayList<String> tokens) throws SQLException;
+    ArrayList<String> filter(ArrayList<String> tokens);
 }

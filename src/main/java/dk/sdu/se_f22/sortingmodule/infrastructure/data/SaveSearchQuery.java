f3e54bdc85bd21f0d54676a4b8294d4bcdc03ba2
Oscar J. C. Johansen <44866812+OJCJ@users.noreply.github.com>
2022-03-22 13:58:08 +0100

    Extract data from SearchQuery object

--- src/main/java/dk/sdu/se_f22/sortingmodule/infrastructure/data/SaveSearchQuery.java
+++ src/main/java/dk/sdu/se_f22/sortingmodule/infrastructure/data/SaveSearchQuery.java
@@ -2,8 +2,14 @@ package dk.sdu.se_f22.sortingmodule.infrastructure.data;
 
 import dk.sdu.se_f22.sortingmodule.infrastructure.domain.SearchQuery;
 
+import java.util.ArrayList;
+
 public class SaveSearchQuery {
     public static void saveSearch(SearchQuery query) {
-        
+        int pageNumber = query.getPagination()[0];
+        int pageSize = query.getPagination()[1];
+        ArrayList<Object> queryRanges = query.getRange();
+        ArrayList<Integer> queryCategories = query.getCategory();
+        int queryScoring = query.getScoring();
     }
 }

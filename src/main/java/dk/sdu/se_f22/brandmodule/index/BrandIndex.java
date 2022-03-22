633bb560d2c7e307f5714e06b4403970ec99a257
Kundayi <kund.jumbe@gmail.com>
2022-03-22 14:37:05 +0100

    Update BrandIndex.java

--- src/main/java/dk/sdu/se_f22/brandmodule/index/BrandIndex.java
+++ src/main/java/dk/sdu/se_f22/brandmodule/index/BrandIndex.java
@@ -4,6 +4,10 @@ import dk.sdu.se_f22.sharedlibrary.db.DBConnection;
 import dk.sdu.se_f22.sharedlibrary.models.Brand;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BrandIndex implements IndexInterface {
@@ -12,7 +16,34 @@ public class BrandIndex implements IndexInterface {
 
     @Override
     public List<Brand> searchBrandIndex(List<String> tokens) {
-        return null;
+        List<Brand> brandList = new ArrayList<>();
+        List<Integer> tempList = new ArrayList<>();
+        String finalQuery = "SELECT * FROM TokenBrandMap LEFT JOIN Brand ON Brand.id = brandId WHERE tokenId = ?";
+
+        try {
+            for(int i = 0; i < tokens.size(); i++) {
+                PreparedStatement queryStatement = DBConn.prepareStatement("SELECT id FROM Tokens WHERE token = ?");
+                queryStatement.setString(1, tokens.get(i));
+                ResultSet queryResultSet = queryStatement.executeQuery();
+                tempList.add(Integer.valueOf(queryResultSet.getString("id")));
+            }
+
+            for (int i = 0; i < tempList.size(); i++) {
+                finalQuery += "AND token = ?";
+            }
+
+            PreparedStatement queryStatement =DBConn.prepareStatement(finalQuery);
+
+            for (int i = 0; i < tempList.size(); i++) {
+                queryStatement.setString(i+1, String.valueOf(tempList.get(i)));
+            }
+
+            ResultSet
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return brandList;
     }
 
     @Override

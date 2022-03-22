17aa9db94a25cae7b3a3ec4bc5e106bbce8a69c2
OFHansen <ohans21@student.sdu.dk>
2022-03-22 12:22:09 +0100

    Changed outputs of CRUD classes to booleans to make tests

--- src/test/java/dk/sdu/se_f22/searchmodule/misspellings/TestSuite.java
+++ src/test/java/dk/sdu/se_f22/searchmodule/misspellings/TestSuite.java
@@ -1,9 +1,11 @@
 package dk.sdu.se_f22.searchmodule.misspellings;
 
 import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
-@SelectClasses({MisspellingsTest.class})
+@SelectClasses(MisspellingsTest.class)
+//@SelectPackages({"misspellings"})
 public class TestSuite {
 }

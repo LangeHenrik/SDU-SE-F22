ff87867d62885f706e2acf0c311dd40e51cee368
Magnus Jensen <magnushjensen.mail@gmail.com>
2022-03-22 13:08:43 +0100

    implement indexingModule interface

--- src/main/java/dk/sdu/se_f22/productmodule/infrastructure/domain/ProductInfSearchImpl.java
+++ src/main/java/dk/sdu/se_f22/productmodule/infrastructure/domain/ProductInfSearchImpl.java
@@ -1,15 +1,22 @@
 package dk.sdu.se_f22.productmodule.infrastructure.domain;
 
+import dk.sdu.se_f22.searchmodule.infrastructure.interfaces.IndexingModule;
 import dk.sdu.se_f22.sharedlibrary.models.Product;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class ProductInfSearchImpl implements ProductInfSearch {
+public class ProductInfSearchImpl implements ProductInfSearch, IndexingModule<Product> {
 
     @Override
     public List<Product> searchProducts(List<String> tokens) {
         // return PIM3.searchProducts(tokens);
+        return new ArrayList<>();
+    }
+
+    @Override
+    public List<Product> queryIndex(List<String> tokens) {
+        // Return PIM3.searchProducts;
         return new ArrayList<>();
     }
 }

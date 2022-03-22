0faae132936756cd1d17f6a525ebbd6e7253791e
Katrine Birk <kabir21@student.sdu.dk>
2022-03-22 11:50:36 +0100

    getDelimiters() updated to get list from database

--- src/main/java/dk/sdu/se_f22/searchmodule/infrastructure/DelimiterSettings.java
+++ src/main/java/dk/sdu/se_f22/searchmodule/infrastructure/DelimiterSettings.java
@@ -1,43 +1,28 @@
 package dk.sdu.se_f22.searchmodule.infrastructure;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import dk.sdu.se_f22.sharedlibrary.db.DBConnection;
+
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DelimiterSettings {
     private List<String> delimiters;
-    private Connection connection = null;
-    private String databaseName;
-    private String username;
-    private String password;
-    static DelimiterSettings instance;
 
-    private DelimiterSettings(){
-        initializeConnection();
-    }
-
-    public static DelimiterSettings getInstance(){
-
-        if (instance == null) {
-            instance = new DelimiterSettings();
-        }
-        return instance;
-    }
-
-    public void initializeConnection(){
-        try {
-            DriverManager.registerDriver(new org.postgresql.Driver());
-            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/"+databaseName,username,password);
-
-        } catch (SQLException | IllegalArgumentException e) {
-            e.printStackTrace(System.err);
-        } finally {
-            if (connection == null) System.exit(-1);
-        }
-    }
     public List<String> getDelimiters() {
-        return delimiters;
+        try {
+            PreparedStatement stmt = DBConnection.getConnection().prepareStatement(
+                    "SELECT delimiter FROM searchtokendelimiters");
+            ResultSet resultSet = stmt.executeQuery();
+            delimiters = new ArrayList<>();
+            while (resultSet.next()) {
+                delimiters.add(resultSet.getString(2));
+            }
+            return delimiters;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     public void appendDelimiters(String delimiter) {

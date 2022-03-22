1ec064a42dbf016f2230cb4a2bd9ef164ba74ff8
Rasmus Helleberg Eriksen <rasm239g@hotmail.com>
2022-03-22 10:30:34 +0100

    This commit contains all SOM-56 tasks, not just the SOM-64 subtask.

---
+++ src/main/java/dk/sdu/se_f22/sortingmodule/infrastructure/presentation/SortingModuleDemo.java
@@ -0,0 +1,12 @@
+package dk.sdu.se_f22.sortingmodule.infrastructure.presentation;
+
+import dk.sdu.se_f22.sortingmodule.infrastructure.domain.SortingModuleImpl;
+
+public class SortingModuleDemo {
+    public static void main (String[] args) {
+        SortingModuleImpl module = new SortingModuleImpl();
+
+        module.searchString("Hello, World!");
+        module.search();
+    }
+}

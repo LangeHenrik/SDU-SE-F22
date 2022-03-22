61c6a0a9da374244732456d7c389f2ebaf1b39e1
Tobias Grum-Schwensen <togru21@student.sdu.dk>
2022-03-22 12:21:28 +0100

    Category Update Methods
    Update methods created.

--- src/main/java/dk/sdu/se_f22/sortingmodule/category/domain/TemporaryConnectionTest.java
+++ src/main/java/dk/sdu/se_f22/sortingmodule/category/domain/TemporaryConnectionTest.java
@@ -1,25 +1,14 @@
 package dk.sdu.se_f22.sortingmodule.category.domain;
 
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
 public class TemporaryConnectionTest {
 
     public static void main(String[] args) {
-        CategoryDBConnection db = CategoryDBConnection.shared;
-        CategoryDBConnection.shared.connect();
+        UpdateCategory updater = new UpdateCategory();
 
-        try {
-            PreparedStatement instertStatement = CategoryDBConnection.shared.getConnie().prepareStatement(
-                    "INSERT INTO Categories(name, description) VALUES (?,?) "
-            );
-            instertStatement.setString(1, "test");
-            instertStatement.setString(2, "En lang test da dette er en beskrivelse");
-            instertStatement.execute();
 
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        CategoryDBConnection.shared.closeConnection();
+        System.out.println(updater.updateParentID(2,3));
+        /*updater.updateName(31,"Kevin");
+        updater.updateDescription(31,"Kevin spiller WoW");*/
+
     }
 }

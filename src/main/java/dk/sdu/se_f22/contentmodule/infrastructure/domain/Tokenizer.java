f7cfc1b18585480a9ec9bbb52f1355b86b67f4bd
zackaad <73196460+zackaad@users.noreply.github.com>
2022-03-22 18:16:04 +0100

    Update Tokenizer.java
    endelig virker tokenizer

--- src/main/java/dk/sdu/se_f22/contentmodule/infrastructure/domain/Tokenizer.java
+++ src/main/java/dk/sdu/se_f22/contentmodule/infrastructure/domain/Tokenizer.java
@@ -1,19 +1,50 @@
 package dk.sdu.se_f22.contentmodule.infrastructure.domain;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Locale;
 
 public class Tokenizer {
-    public static ArrayList<String> tokenizeString(String text) {
-        String[] splittedString = text.split("[-.,;:_ ]");
+
+    static HashMap<Integer, String> HTMLSites;
+
+    public Tokenizer(HashMap<Integer, String> HTMLSites) {
+        this.HTMLSites = HTMLSites;
+    }
+
+
+    public static ArrayList<String> tokenizeHTMLBodyText(HashMap HTMLSites){
+
+
+        ArrayList<String> bodyText = new ArrayList<>();
+        String[] splittedString;
         ArrayList<String> tokens = new ArrayList();
 
-        for (String s : splittedString) {
-            if (s != "") {
-                String string = s.toLowerCase(Locale.ROOT);
-                tokens.add(string);
+        for (Object theValues: HTMLSites.values()){
+            bodyText.add((String) theValues);
+        }
+
+        for (int i = 0; i < bodyText.size() ; i++) {
+            splittedString = bodyText.get(i).split("[-.,;:_!?]");
+            for (String s : splittedString) {
+                if (s != "") {
+                    String string = s.toLowerCase(Locale.ROOT);
+                    tokens.add(string);
+                }
             }
         }
+
+
         return tokens;
     }
-}
+
+
+
+
+    }
+
+
+
+
+
+

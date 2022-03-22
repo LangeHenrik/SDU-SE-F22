50e31db6f1ae57ed394116fe31a9471e7c1d5793
Mads Pedersen <mads456dk@gmali.com>
2022-03-22 13:49:33 +0100

    Clean and refactored
    Cleaned and refactored DelimiterSettings.java

--- src/main/java/dk/sdu/se_f22/searchmodule/infrastructure/DelimiterSettings.java
+++ src/main/java/dk/sdu/se_f22/searchmodule/infrastructure/DelimiterSettings.java
@@ -12,51 +12,75 @@ public class DelimiterSettings {
 
     public List<String> getDelimiters() {
         try {
-            PreparedStatement stmt = DBConnection.getConnection().prepareStatement(
-                    "SELECT * FROM searchtokendelimiters");
-            ResultSet resultSet = stmt.executeQuery();
-            delimiters = new ArrayList<>();
-            while (resultSet.next()) {
-                delimiters.add(resultSet.getString(1));
-            }
+            updateDelimitersToDatabaseState();
             return delimiters;
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
             return null;
         }
     }
 
+    private void updateDelimitersToDatabaseState() throws SQLException {
+        ResultSet resultSet = getAllDelimitersFromDatabase();
+        resetDelimiterAttribute();
+        moveResultsSetToDelimitersAttribute(resultSet);
+        resultSet.close();
+    }
+
+    private ResultSet getAllDelimitersFromDatabase() throws SQLException {
+        PreparedStatement stmt = DBConnection.getConnection().prepareStatement("SELECT * FROM searchtokendelimiters");
+        return stmt.executeQuery();
+    }
+
+    private void resetDelimiterAttribute() {
+        delimiters = new ArrayList<>();
+    }
+
+    private void moveResultsSetToDelimitersAttribute(ResultSet resultSet) throws SQLException {
+        while (resultSet.next()) {
+            delimiters.add(resultSet.getString(1));
+        }
+    }
+
     public void addDelimiter(String delimiter) {
         try {
-            PreparedStatement stmt = DBConnection.getConnection().prepareStatement(
-                    "INSERT INTO searchtokendelimiters (delimiter) VALUES (?)");
-            stmt.setString(1, delimiter);
-            stmt.execute();
-
+            insertDelimiterIntoDatabase(delimiter);
             System.out.println("Delimiter added.");
-        } catch (PSQLException ex){
+        } catch (PSQLException ex) {
             System.out.println("This delimiter already exist");
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
+    private void insertDelimiterIntoDatabase(String delimiter) throws SQLException {
+        PreparedStatement stmt = prepareDeleteStatement("INSERT INTO searchtokendelimiters (delimiter) VALUES (?)", delimiter);
+        stmt.execute();
+    }
+
     public boolean removeDelimiter(String delim) {
         try {
-            PreparedStatement stmt = DBConnection.getConnection().prepareStatement(
-                    "DELETE FROM searchtokendelimiters WHERE delimiter=?"
-            );
-            stmt.setString(1, delim);
-            for (String s : getDelimiters()) {
-                if (s.equals(delim)) {
-                    stmt.execute();
-                    return true;
-                }
-            }
+            PreparedStatement stmt = prepareDeleteStatement("DELETE FROM searchtokendelimiters WHERE delimiter=?", delim);
+            return executeStatementIfDelimIsInDelimiters(delim, stmt);
         } catch (SQLException e) {
             e.printStackTrace();
+            return false;
+        }
+    }
+
+    private boolean executeStatementIfDelimIsInDelimiters(String delim, PreparedStatement stmt) throws SQLException {
+        for (String s : getDelimiters()) {
+            if (s.equals(delim)) {
+                stmt.execute();
+                return true;
+            }
         }
         return false;
+    }
+
+    private PreparedStatement prepareDeleteStatement(String sql, String delim) throws SQLException {
+        PreparedStatement stmt = DBConnection.getConnection().prepareStatement(sql);
+        stmt.setString(1, delim);
+        return stmt;
     }
 }

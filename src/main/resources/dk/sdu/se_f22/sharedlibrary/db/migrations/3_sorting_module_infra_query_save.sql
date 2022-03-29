9174b0fbd336774cb0caa6ccd0387fdd81f3b43e
Mikkel Albrechtsen <mikkel.alb@gmail.com>
2022-03-29 13:26:20 +0200

    Added database for sorting module infrastructure

--- src/main/resources/dk/sdu/se_f22/sharedlibrary/db/migrations/3_sorting_module_infra_query_save.sql
+++ src/main/resources/dk/sdu/se_f22/sharedlibrary/db/migrations/3_sorting_module_infra_query_save.sql
@@ -1,17 +1,17 @@
-CREATE TABLE queries (
+CREATE TABLE sorting_queries (
 	id SERIAL PRIMARY KEY,
 	text VARCHAR(512) NOT NULL,
 	date_stamp TIMESTAMP NOT NULL
 );
 
-CREATE TABLE queries_settings (
+CREATE TABLE sorting_query_settings (
 	queries_id INT REFERENCES queries(id),
 	key VARCHAR(255) NOT NULL,
 	value VARCHAR(255) NOT NULL,
 	PRIMARY KEY (queries_id, key)
 );
 
-CREATE TABLE queries_category(
+CREATE TABLE sorting_query_category(
 	queries_id INT REFERENCES queries(id),
 	catergory_id INT NOT NULL,
 	PRIMARY KEY (queries_id, catergory_id)

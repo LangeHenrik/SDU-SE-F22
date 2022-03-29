496a3a119a0b4840ffc9dd9cfcb0abeff9602b7a
Eivan20 <eivan20@student.sdu.dk>
2022-03-29 15:02:38 +0200

    Implemented readEntireDB

--- src/main/java/dk/sdu/se_f22/searchmodule/onewaysynonyms/DatabaseAPI.java
+++ src/main/java/dk/sdu/se_f22/searchmodule/onewaysynonyms/DatabaseAPI.java
@@ -3,13 +3,14 @@ package dk.sdu.se_f22.searchmodule.onewaysynonyms;
 import dk.sdu.se_f22.sharedlibrary.db.DBConnection;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class DatabaseAPI {
 
     static Connection connection = DBConnection.getConnection();
     //for adding superItem
 
-        public static void addItem(String itemName) throws SQLException {
+    public static void addItem(String itemName) throws SQLException {
         PreparedStatement insertStatement = null;
         insertStatement = connection.prepareStatement("INSERT INTO items (name,superId) VALUES (?,?)");
         insertStatement.setString(1, itemName);
@@ -41,20 +42,47 @@ public class DatabaseAPI {
         updateStatement.execute();
     }
 
-    public static ResultSet read() {
+    public static Item[] readEntireDB() {
         try {
+            ArrayList<Item> items = new ArrayList<Item>();
             PreparedStatement quaryStatement = connection.prepareStatement("SELECT * FROM items");
             ResultSet quaryResultSet = null;
             quaryResultSet = quaryStatement.executeQuery();
-
-            return quaryResultSet;
+            while (quaryResultSet.next()) {
+                items.add(new Item(quaryResultSet.getInt(1), quaryResultSet.getString(2), quaryResultSet.getInt(3)));
+            }
+            for (Item item : items) {
+                if (item.getSuperId() != 0) {
+                    for (Item item2 : items) {
+                        if (item2.getId() == item.getSuperId())
+                            item.setSuperItem(item2);
+                    }
+                }
+            }
+            Item[] item = items.toArray(new Item[items.size()]);
+            return item;
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return null;
     }
 
-    public static void deleteItem(int id, String name){
+    public static ResultSet readDB(String name) {
+        try {
+            PreparedStatement quaryStatement = connection.prepareStatement("SELECT * FROM items WHERE name = ?");
+            quaryStatement.setString(1, name);
+            ResultSet quaryResultSet = quaryStatement.executeQuery();
+            while (quaryResultSet.next()) {
+                if (quaryResultSet.getInt(1) != 0) {
+
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void deleteItem(int id, String name) {
         PreparedStatement deleteStatement = null;
         try {
             deleteStatement = connection.prepareStatement("DELETE FROM items WHERE name=? AND id=?");

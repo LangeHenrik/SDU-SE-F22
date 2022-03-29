720055e2bf838c4195041b4de5477b6f99609acb
LaugeSolvang <lasol21@student.sdu.dk>
2022-03-29 12:27:23 +0200

    Added test for Scoring

---
+++ src/test/java/dk/sdu/se_f22/sortingmodule/scoring/ScoringTest.java
@@ -0,0 +1,51 @@
+package dk.sdu.se_f22.sortingmodule.scoring;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ScoringTest {
+    @Test
+    void wrapProduct() {
+    }
+
+    @Test
+    void unWrapProduct() {
+    }
+
+    @Test
+    void scoreSort() {
+    }
+
+    @Test
+    void scoreSortPrice() {
+    }
+
+    @Test
+    void scoreSortReview() {
+    }
+
+    @Test
+    void scoreSortStock() {
+    }
+
+    @Test
+    void scoreSortDate() {
+    }
+
+    @Test
+    void readTable() {
+    }
+
+    @Test
+    void updateRow() {
+    }
+
+    @Test
+    void deleteRow() {
+    }
+
+    @Test
+    void createRow() {
+    }
+}

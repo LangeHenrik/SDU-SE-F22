ce200f53eda3f19eb50d178eb1401df910cafd7e
Storm311 <57753583+Storm311@users.noreply.github.com>
2022-03-29 14:06:06 +0200

    Update Main.java

--- src/main/java/dk/sdu/se_f22/Main.java
+++ src/main/java/dk/sdu/se_f22/Main.java
@@ -1,11 +1,6 @@
 package dk.sdu.se_f22;
 
 
-import dk.sdu.se_f22.brandmodule.index.BrandIndex;
-import dk.sdu.se_f22.sharedlibrary.db.DBConnection;
-import dk.sdu.se_f22.sharedlibrary.models.Brand;
-
-import java.sql.Connection;
 
 import dk.sdu.se_f22.sharedlibrary.db.LoggingProvider;
 import org.apache.logging.log4j.Logger;

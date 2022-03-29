5e9752210b0e568bd5ffcbe574fe1d0d16f47dca
XBetaHunterX <BrutalBetaHunter@gmail.com>
2022-03-29 08:49:28 +0200

    Moved docstrings from reader and deleter interface to read and delete methods under CRUDInterface.

--- src/main/java/dk/sdu/se_f22/sortingmodule/range/rangepublic/crud/RangeFilterCRUDInterface.java
+++ src/main/java/dk/sdu/se_f22/sortingmodule/range/rangepublic/crud/RangeFilterCRUDInterface.java
@@ -13,7 +13,21 @@ public interface RangeFilterCRUDInterface {
     RangeFilter create(String description, String name, String productAttribute, long dbMinToSave, long dbMaxToSave) throws InvalidFilterException;
     RangeFilter create(String description, String name, String productAttribute, Instant dbMinToSave, Instant dbMaxToSave) throws InvalidFilterException;
 
+    /**
+     * Method first reads a filter from the database.
+     * It then returns the filter as a RangeFilter.
+     * @param id
+     * @return RangeFilter
+     */
     RangeFilter read(int id) throws IdNotFoundException, UnknownFilterTypeException;
+
+    /**
+     * This method uses the database delete method, which goes into the database <br>
+     * and deletes the filter corresponding with the id given as a parameter. <br>
+     * Throws IdNotFoundException
+     * @param id Integer
+     * @return RangeFilter
+     */
     RangeFilter delete(int id) throws IdNotFoundException;
 
     RangeFilter update(RangeFilter filter, String newName) throws InvalidFilterException;
@@ -22,5 +36,10 @@ public interface RangeFilterCRUDInterface {
     RangeFilter update(RangeFilter filter, long dbMinToSave, long dbMaxToSave) throws InvalidFilterException;
     RangeFilter update(RangeFilter filter, Instant dbMinToSave, Instant dbMaxToSave) throws InvalidFilterException;
 
+    /**
+     * Method first reads all filters from the database.
+     * It then returns all filters in a list.
+     * @return List<RangeFilter>
+     */
     List<RangeFilter> readAll();
 }

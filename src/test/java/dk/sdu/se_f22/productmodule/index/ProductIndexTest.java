b1811dd41aa9eb8cd43d22dec4bdf87304c5fc89
Nikolaj <nstau21@student.sdu.dk>
2022-03-29 11:46:14 +0200

    set up test

--- src/test/java/dk/sdu/se_f22/productmodule/index/ProductIndexTest.java
+++ src/test/java/dk/sdu/se_f22/productmodule/index/ProductIndexTest.java
@@ -1,7 +1,21 @@
 package dk.sdu.se_f22.productmodule.index;
 
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductIndexTest {
+
+    @BeforeAll
+    static void setup(){
+        System.out.println("@BeforeAll executed");
+    }
+
+    @AfterAll
+    static void tearDown(){
+        System.out.println("@AfterAll executed");
+    }
 
 }

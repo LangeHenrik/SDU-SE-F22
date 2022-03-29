19f2c47c8a5718c1d5371dd19c36e2680ad7c5a3
OFHansen <ohans21@student.sdu.dk>
2022-03-29 09:35:05 +0200

    Better tests

--- src/main/java/dk/sdu/se_f22/searchmodule/misspellings/Misspellings.java
+++ src/main/java/dk/sdu/se_f22/searchmodule/misspellings/Misspellings.java
@@ -2,16 +2,15 @@ package dk.sdu.se_f22.searchmodule.misspellings;
 
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Misspellings implements DatabaseOperator {
-    private final String url = "jdbc:postgresql://abul.db.elephantsql.com/hzajyqbo";
-    private final String user = "hzajyqbo";
-    private final String password = "K8664qtGojuBvQczzv66EhaqkUNbXLj0";
+    private static final String url = "jdbc:postgresql://abul.db.elephantsql.com/hzajyqbo";
+    private static final String user = "hzajyqbo";
+    private static final String password = "K8664qtGojuBvQczzv66EhaqkUNbXLj0";
     private static final String QUERY = "SELECT wrong, correct FROM misspellings WHERE wrong =?";
-    private static final String INSERT_misspellings = "INSERT INTO misspellings (wrong, correct) VALUES (?,?);";
-    private static final String UPDATE_misspellings = "UPDATE misspellings misspellings SET wrong=? WHERE wrong=?";
-    private static final String DELETE_misspellings = "DELETE FROM misspellings WHERE wrong = ?";
+    private static final String INSERT_MISSPELLINGS = "INSERT INTO misspellings (wrong, correct) VALUES (?,?);";
+    private static final String UPDATE_MISSPELLINGS = "UPDATE misspellings misspellings SET wrong=? WHERE wrong=?";
+    private static final String DELETE_MISSPELLINGS = "DELETE FROM misspellings WHERE wrong = ?";
 
 
     /*
@@ -24,12 +23,15 @@ public class Misspellings implements DatabaseOperator {
         for (int i = 0; i < tokens.size() ; i++) {
             // Step 1: Establishing a Connection
             try (Connection connection = DriverManager.getConnection(url, user, password);
+
                  // Step 2:Create a statement using connection object
                  PreparedStatement preparedStatement = connection.prepareStatement(QUERY);) {
                 preparedStatement.setString(1, tokens.get(i));
                 System.out.println(preparedStatement);
+
                 // Step 3: Execute the query or update query
                 ResultSet rs = preparedStatement.executeQuery();
+
                 // Step 4: Process the ResultSet object.
                 while (rs.next()) {
                     String correctWord = rs.getString("correct");
@@ -49,16 +51,17 @@ public class Misspellings implements DatabaseOperator {
 
     @Override
     public boolean addMisspelling(String wrong, String correct) {
-        //add the new misspelling
+
         // Step 1: Establishing a Connection
         try (Connection connection = DriverManager.getConnection(url, user, password);
+
              // Step 2:Create a statement using connection object
-             PreparedStatement preparedStatement = connection.prepareStatement(INSERT_misspellings)) {
+             PreparedStatement preparedStatement = connection.prepareStatement(INSERT_MISSPELLINGS)) {
             preparedStatement.setString(1, wrong);
             preparedStatement.setString(2, correct);
 
             System.out.println(preparedStatement);
-            //System.out.println("The misspelling: "+misspelling+" have been added."+" The correct spelling is "+correct);
+
             // Step 3: Execute the query or update query
             preparedStatement.executeUpdate();
             return true;
@@ -72,19 +75,20 @@ public class Misspellings implements DatabaseOperator {
     public boolean deleteMisspelling(String misspelling) {
         try {
             Connection con = DriverManager.getConnection(url, user, password);
-            String SQL = "SELECT wrong FROM misspellings WHERE wrong=?";
-            PreparedStatement statement = con.prepareStatement(SQL);
+            PreparedStatement statement = con.prepareStatement(QUERY);
             statement.setString(1,misspelling);
 
             ResultSet rs = statement.executeQuery();
             if (rs.next()){
                 // Step 1: Establishing a Connection
                 try (Connection connection = DriverManager.getConnection(url, user, password);
+
                      // Step 2:Create a statement using connection object
-                     PreparedStatement preparedStatement = connection.prepareStatement(DELETE_misspellings)) {
+                     PreparedStatement preparedStatement = connection.prepareStatement(DELETE_MISSPELLINGS)) {
                     preparedStatement.setString(1, misspelling);
 
                     System.out.println(preparedStatement);
+
                     // Step 3: Execute the query or update query
                     preparedStatement.execute();
                     return true;
@@ -107,8 +111,7 @@ public class Misspellings implements DatabaseOperator {
     public boolean updateMisspelling(String oldMisspelling, String newMisspelling) {
         try {
             Connection con = DriverManager.getConnection(url, user, password);
-            String SQL = "SELECT wrong FROM misspellings WHERE wrong=?";
-            PreparedStatement statement = con.prepareStatement(SQL);
+            PreparedStatement statement = con.prepareStatement(QUERY);
             statement.setString(1,oldMisspelling);
 
             ResultSet rs = statement.executeQuery();
@@ -117,12 +120,14 @@ public class Misspellings implements DatabaseOperator {
 
                 // Step 1: Establishing a Connection
                 try (Connection connection = DriverManager.getConnection(url, user, password);
+
                      // Step 2:Create a statement using connection object
-                     PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_misspellings)) {
+                     PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_MISSPELLINGS)) {
                     preparedStatement.setString(1, newMisspelling);
                     preparedStatement.setString(2, oldMisspelling);
 
                     System.out.println(preparedStatement);
+
                     // Step 3: Execute the query or update query
                     preparedStatement.executeUpdate();
                     return true;
@@ -140,6 +145,7 @@ public class Misspellings implements DatabaseOperator {
         }
 
     }
+
 
 
 
--- src/test/java/dk/sdu/se_f22/searchmodule/misspellings/MisspellingsTest.java
+++ src/test/java/dk/sdu/se_f22/searchmodule/misspellings/MisspellingsTest.java
@@ -2,62 +2,100 @@ package dk.sdu.se_f22.searchmodule.misspellings;
 
 import org.junit.jupiter.api.*;
 
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MisspellingsTest {
     Misspellings misspelling = new Misspellings();
+    private static final String url = "jdbc:postgresql://abul.db.elephantsql.com/hzajyqbo";
+    private static final String user = "hzajyqbo";
+    private static final String password = "K8664qtGojuBvQczzv66EhaqkUNbXLj0";
+
+    ArrayList<String> listWrong = new ArrayList<String>();
+    ArrayList<String> listCorrect = new ArrayList<String>();
+
+
 
     @BeforeEach
     void setUp() {
-    }
-
-   @Test
-    void filter() {
-        ArrayList<String> listWrong = new ArrayList<String>();
+        try (Connection connection = DriverManager.getConnection(url, user, password)){
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement("DELETE FROM misspellings");
+            preparedStatement.execute();
+            PreparedStatement filterTest1 =
+                    connection.prepareStatement("INSERT INTO misspellings (wrong, correct) VALUES ('HAJ','HEJ') ");
+            PreparedStatement filterTest2 =
+                    connection.prepareStatement("INSERT INTO misspellings (wrong, correct) VALUES ('HIJ','HEJ') ");
+            filterTest1.execute();
+            filterTest2.execute();
+            PreparedStatement testMisspelling =
+                    connection.prepareStatement("INSERT INTO misspellings (wrong, correct) VALUES ('TestMisspelling','MisspellingTest') ");
+            testMisspelling.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        //Wrong methods for filter method
         listWrong.add("HEJ");
         listWrong.add("HAJ");
         listWrong.add("HIJ");
         listWrong.add("HEJ");
-
-        ArrayList<String> listCorrect = new ArrayList<String>();
+        //Correct words for filter method
         listCorrect.add("HEJ");
         listCorrect.add("HEJ");
         listCorrect.add("HEJ");
         listCorrect.add("HEJ");
-
-
-       assertEquals(listCorrect, misspelling.filter(listWrong));
-   }
-
-    @DisplayName("Tests adding misspelling, " +
-            "tests for duplicate, tests for deletion, tests for deletion of non existant misspelling.")
-    @Test
-    void addingMisspellingsAndRemovingMisspelling() {
-        //Adds missepelling
-        assertTrue(misspelling.addMisspelling("HRRJRJRJJ","HEJ"));
-        //Tries adding same misspelling
-        assertFalse(misspelling.addMisspelling("HRRJRJRJJ","HEJ"));
-        //Deletes misspelling again
-        assertTrue(misspelling.deleteMisspelling("HRRJRJRJJ"));
-        //Attempts to delete the same misspelling again
-        assertFalse(misspelling.deleteMisspelling("HRRJRJRJJ"));
     }
-
-    @DisplayName("Adds misspelling, updates it, then deletes it.")
-    @Test
-    void updateMisspellings() {
-        misspelling.addMisspelling("JAGA","JAVA");
-        assertTrue(misspelling.updateMisspelling("JAGA","JARA"));
-        assertFalse(misspelling.updateMisspelling("JAGA","JARA"));
-        misspelling.deleteMisspelling("JARA");
-    }
-
-
 
     @AfterEach
     void tearDown() {
+        listWrong.clear();
+        listCorrect.clear();
     }
+
+
+    @DisplayName("Tests the filter method my comparing two ArrayLists, to misspellings saved in the database during setup.")
+    @Test
+    void filter() {
+       assertEquals(listCorrect, misspelling.filter(listWrong));
+   }
+
+    @DisplayName("Tests adding misspelling.")
+    @Test
+    void addingMisspelling() {
+        assertTrue(misspelling.addMisspelling("HRRJRJRJJ","HEJ"));
+    }
+
+    @DisplayName("Tries adding the testMisspelling from the setUp, creating a duplicate misspelling error.")
+    @Test
+    void addingDuplicateMisspelling() {
+        assertFalse(misspelling.addMisspelling("TestMisspelling","MisspellingTest"));
+    }
+
+    @DisplayName("Tries deleting the testMisspelling from the setUp.")
+    @Test
+    void deleteMisspelling() {
+        assertTrue(misspelling.deleteMisspelling("TestMisspelling"));
+    }
+
+    @DisplayName("Tries to delete a non existant misspelling.")
+    @Test
+    void deleteNonExistantMisspelling() {
+        assertFalse(misspelling.deleteMisspelling("HRRJRJRJJ"));
+    }
+
+    @DisplayName("Updates the testMisspelling from setUp.")
+    @Test
+    void updateMisspellings() {
+        assertTrue(misspelling.updateMisspelling("TestMisspelling","TestOfMisspelling"));
+    }
+
+    @DisplayName("Tries updating a non existant misspelling.")
+    @Test
+    void updateNonExistantMisspelling() {
+        assertFalse(misspelling.updateMisspelling("nonExistant","Test"));
+    }
+
+
 }

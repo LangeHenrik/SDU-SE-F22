b1d1c477b46e9f1de139e23156e65033789e3198
impostermp <37489999+impostermp@users.noreply.github.com>
2022-04-05 10:09:25 +0200

    Update ProductIndex.java

--- src/main/java/dk/sdu/se_f22/productmodule/index/ProductIndex.java
+++ src/main/java/dk/sdu/se_f22/productmodule/index/ProductIndex.java
@@ -17,6 +17,39 @@ public class ProductIndex implements IProductIndex{
     private int descriptionHits = 0;
     private List<ProductHit> sortedList = new ArrayList<>();
 
+    private String url = "localhost";
+    private int port = 5432;
+    private String databaseName = "xxxx";
+    private String username = "xxxx";
+    private String password = "xxxx";
+    private Connection connection = null;
+    private static ProductIndex instance;
+
+    private ProductIndex(){
+        initializePostgresqlDatabase();
+    }
+
+    public static CreateReadClass getInstance(){
+        if (instance == null) {
+            instance = new CreateReadClass();
+        }
+        return instance;
+    }
+
+    private void initializePostgresqlDatabase() {
+        try {
+            DriverManager.registerDriver(new org.postgresql.Driver());
+            connection = DriverManager.getConnection("jdbc:postgresql://" + url
+                    + ":"
+                    + port
+                    + "/" + databaseName, username, password);
+        } catch (SQLException | IllegalArgumentException ex) {
+            ex.printStackTrace(System.err);
+        } finally {
+            if (connection == null) System.exit(-1);
+        }
+    }
+
     // Method for finding amount of hits within a product by a token, then returning an indexed list by the hits
     public List<ProductHit> indexProductsByToken(List<ProductHit> products, List<String> token) {
 
@@ -95,5 +128,53 @@ public class ProductIndex implements IProductIndex{
             e.printStackTrace();
         }
 
+    }
+    public void createProduct(ProductHit product){
+        try {
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement("INSERT into products (uuid,averageuserreview,price,clockspeed,weight,ean,size,category, name,description,publisheddate,expirationdate,inStock) values (?,?,?,?,?,?,?,?,?,?,?,?,?)");
+            preparedStatement.setObject(1,product.uuid);
+            preparedStatement.setDouble(2,product.averageUserReview);
+            preparedStatement.setDouble(3,product.price);
+            preparedStatement.setDouble(4,product.clockspeed);
+            preparedStatement.setDouble(5,product.weight);
+            preparedStatement.setString(6,product.ean);
+            preparedStatement.setString(7,product.size);
+            preparedStatement.setString(8,product.category);
+            preparedStatement.setString(9,product.name);
+            preparedStatement.setString(10,product.description);
+            preparedStatement.setTimestamp(11,Timestamp.from(product.publishedDate));
+            preparedStatement.setTimestamp(12,Timestamp.from(product.expirationDate));
+            preparedStatement.setArray(13, connection.createArrayOf("VARCHAR",
+                    product.inStock.toArray()));
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public List<ProductHit> getProducts(){
+        List<ProductHit> productHitList = new ArrayList<>();
+        try {
+            PreparedStatement p = connection.prepareStatement("select * from products");
+            ResultSet resultSet = p.executeQuery();
+            while(resultSet.next()){
+                productHitList.add(new ProductHit(UUID.fromString(resultSet.getString(2)),
+                        resultSet.getDouble(3),
+                        resultSet.getDouble(4),
+                        resultSet.getDouble(5),
+                        resultSet.getDouble(6),
+                        resultSet.getString(7),
+                        resultSet.getString(8),
+                        resultSet.getString(9),
+                        resultSet.getString(10),
+                        resultSet.getString(11),
+                        resultSet.getTimestamp(12).toInstant(),
+                        resultSet.getTimestamp(13).toInstant(),
+                        Arrays.asList(resultSet.getArray(14).toString())));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return productHitList;
     }
 }

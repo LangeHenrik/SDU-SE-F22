baffe61138e0144588d7f1d05b206cf0d97fc6e4
AlexBirkwald <abirkwald@gmail.com>
2022-04-19 09:54:39 +0200

    Cleaned up Main Class, removing redundant code

--- src/main/java/dk/sdu/se_f22/productmodule/index/Main.java
+++ src/main/java/dk/sdu/se_f22/productmodule/index/Main.java
@@ -9,10 +9,5 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         ProductIndex p = ProductIndex.getInstance();
-        // create list of strings
-        List<String> str = new ArrayList<>();
-        str.add("HP");
-
-
     }
 }

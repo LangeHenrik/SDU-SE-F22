d0a7af482fef04976fa9703de68040f2e5ee1b59
DominicNr2 <dongu21@student.sdu.dk>
2022-04-19 13:32:17 +0200

    small changes to administrator gui

---
+++ src/test/java/dk/sdu/se_f22/searchmodule/infrastructure/SEM1InfrastructureAdministrativeGUIControllerTest.java
@@ -0,0 +1,22 @@
+package dk.sdu.se_f22.searchmodule.infrastructure;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+@ExtendWith(ApplicationExtension.class)
+class SEM1InfrastructureAdministrativeGUIControllerTest {
+
+    @Test
+    void initialize() {
+    }
+
+    @Test
+    void addBtn() {
+    }
+
+    @Test
+    void removeBtn() {
+    }
+}

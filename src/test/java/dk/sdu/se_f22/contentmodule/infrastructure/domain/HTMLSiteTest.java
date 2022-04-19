0015ac56ec7afb3fa2566aa0e577f8f8103bc650
zackaad <73196460+zackaad@users.noreply.github.com>
2022-04-19 12:34:28 +0200

    HTMlSite class and Tokenizer class tested.

--- src/test/java/dk/sdu/se_f22/contentmodule/infrastructure/domain/HTMLSiteTest.java
+++ src/test/java/dk/sdu/se_f22/contentmodule/infrastructure/domain/HTMLSiteTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-//done
+//done2
 class HTMLSiteTest {
     HTMLSite site;
 
--- src/test/java/dk/sdu/se_f22/contentmodule/infrastructure/domain/TokenizerTest.java
+++ src/test/java/dk/sdu/se_f22/contentmodule/infrastructure/domain/TokenizerTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 
-//done
+//done2
 class TokenizerTest {
     Tokenizer tk;
     HTMLSite site;

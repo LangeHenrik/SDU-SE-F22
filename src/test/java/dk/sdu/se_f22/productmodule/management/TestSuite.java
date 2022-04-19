596416cd8174aae807db091c78ef560b87ad3285
Marcus Schebye Skrvier <marcus@familienskriver.dk>
2022-04-19 14:15:49 +0200

    Fixed Test Suite

--- src/test/java/dk/sdu/se_f22/productmodule/management/TestSuite.java
+++ src/test/java/dk/sdu/se_f22/productmodule/management/TestSuite.java
@@ -5,8 +5,8 @@ import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
-//@SelectClasses({ProductManagerTest.class, ProductAttributeTest.class, JSONReaderTest.class, ProductTest.class})
-@SelectPackages({"dk.sdu.se_f22.productmodule.management"})
+@SelectClasses({ProductManagerTest.class, ProductAttributeTest.class, JSONReaderTest.class, ProductTest.class})
+//@SelectPackages({"dk.sdu.se_f22.productmodule.management"})
 public class TestSuite {
 
 }

3ab53dfdf29c0b8b537c416a4918a388e2032f18
Patrick <Patrickbjerre@outlook.dk>
2022-04-19 13:42:20 +0200

    Made the Index Test

--- src/main/java/dk/sdu/se_f22/Main.java
+++ src/main/java/dk/sdu/se_f22/Main.java
@@ -1,3 +1,13 @@
 package dk.sdu.se_f22;
+
+import dk.sdu.se_f22.brandmodule.management.services.IndexingService;
+
+import java.security.Provider;
+
 public class Main {
+    public static void main(String[] args) {
+        IndexingService service = new IndexingService();
+
+        service.StartIndexInterval();
+    }
 }

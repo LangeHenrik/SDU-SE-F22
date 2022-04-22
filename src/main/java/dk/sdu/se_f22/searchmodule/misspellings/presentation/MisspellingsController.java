a4ca00b5c97c9b74ca563ddffb1e2fa41b2fc192
OFHansen <ohans21@student.sdu.dk>
2022-04-22 13:53:44 +0200

    FXML variables are dumb

--- src/main/java/dk/sdu/se_f22/searchmodule/misspellings/presentation/MisspellingsController.java
+++ src/main/java/dk/sdu/se_f22/searchmodule/misspellings/presentation/MisspellingsController.java
@@ -1,19 +1,16 @@
 package dk.sdu.se_f22.searchmodule.misspellings.presentation;
 
 import dk.sdu.se_f22.searchmodule.misspellings.Misspellings;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 import java.util.ArrayList;
 
 public class MisspellingsController {
-    private Misspellings misspelling;
 
-    @FXML
-    Button add, update, delete, filter;
+    private Misspellings misspelling;
 
     @FXML
     TextField misInput, corInput, misUpInput, filterInput;
@@ -21,21 +18,23 @@ public class MisspellingsController {
     @FXML
     TextArea output, outputFilter;
 
-    @FXML
-    Label lMis, lCor, lUpMis, lOut, lOutFilter, lFilterInput;
+    public void initialize(){
+        misspelling = new Misspellings();
+    }
 
-    public void addMis(){
+    public void addMis(ActionEvent e){
+        System.out.println("add works");/*
         if (!misInput.getText().isEmpty() && !corInput.getText().isEmpty()){
             misspelling.addMisspelling(misInput.getText(), corInput.getText());
             output.setText("The misspelling: "+misInput.getText()+" has been added. The correct spelling is: "+corInput.getText());
         } else{
             output.setText("Write a misspelling and it's correct spelling");
-        }
+        }*/
 
     }
 
-    public void updateMis(){
-        if (!misInput.getText().isEmpty() && !misUpInput.getText().isEmpty()){
+    public void updateMis(ActionEvent e){
+       /* if (!misInput.getText().isEmpty() && !misUpInput.getText().isEmpty()){
             misspelling.updateMisspelling(misInput.getText(), misUpInput.getText());
             output.setText("The misspelling: "+misInput.getText()+" has been updated. The the new spelling is: "+misUpInput.getText());
         }else if (!corInput.getText().isEmpty()){
@@ -43,20 +42,20 @@ public class MisspellingsController {
         }
         else{
             output.setText("Write what misspelling you want to update and what misspelling it should be instead");
-        }
+        }*/
     }
 
-    public void deleteMis(){
-        if (!misInput.getText().isEmpty() && misUpInput.getText().isEmpty() && corInput.getText().isEmpty()) {
+    public void deleteMis(ActionEvent e){
+       /* if (!misInput.getText().isEmpty() && misUpInput.getText().isEmpty() && corInput.getText().isEmpty()) {
             misspelling.deleteMisspelling(misInput.getText());
             output.setText("The misspelling: " + misUpInput.getText() + " has been deleted");
         }else{
             output.setText("You only have to write what misspelling you want to delete");
-        }
+        }*/
     }
 
-    public void filterSearch(){
-        if (!filterInput.getText().isEmpty()){
+    public void filterSearch(ActionEvent e){
+        /*if (!filterInput.getText().isEmpty()){
             ArrayList<String> arrayList = new ArrayList();
             for (String wordsIn: arrayList) {
                 filterInput.getText().split(" ");
@@ -66,7 +65,7 @@ public class MisspellingsController {
             for (String wordsOut: arrayList) {
                 outputFilter.setText(wordsOut);
             }
-        }
+        }*/
     }
 
 

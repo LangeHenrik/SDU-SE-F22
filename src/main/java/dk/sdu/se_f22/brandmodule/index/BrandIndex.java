ca90e7d87006b47bc5e18cdff129e8b286e76f90
Frederik Storm <stormfrederik@gmail.com>
2022-04-26 11:24:01 +0200

    Made changes from request in github

--- src/main/java/dk/sdu/se_f22/brandmodule/index/BrandIndex.java
+++ src/main/java/dk/sdu/se_f22/brandmodule/index/BrandIndex.java
@@ -30,23 +30,44 @@ public class BrandIndex implements IndexInterface {
 
             ArrayList<String> newTokens = new ArrayList<>(tokens);
 
-            for (int i = 0; i < tokens.size(); i++){
-                while (rs.next()){
-                    if (rs.getString("token") != newTokens.get(i)){
+            String dbColumnToken = "token"; //
+
+            PreparedStatement queryTokenId = null;
+            ResultSet rsTokenId = null;
+
+
+            for (int i = 0; i < tokens.size(); i++) {
+                while (rs.next()) {
+                    if (rs.getString(dbColumnToken) != newTokens.get(i)) {
                         tokenInsert.setString(1, newTokens.get(i));
                     }
                 }
-                PreparedStatement queryTokenId = DBConn.prepareStatement("SELECT id FROM tokens where token = ?");
-                queryTokenId.setString(1, newTokens.get(i));
-                ResultSet rsTokenId = queryTokenId.executeQuery();
-                while (rsTokenId.next()){
+                tokenInsert.execute();
+            }
+
+
+            for (int j = 0; j < tokens.size(); j++) {
+                queryTokenId = DBConn.prepareStatement("SELECT id FROM tokens where token = ?");
+                queryTokenId.setString(1, newTokens.get(j));
+                rsTokenId = queryTokenId.executeQuery();
+                while (rsTokenId.next()) {
                     mapInsert.setInt(1, brand.getId());
                     mapInsert.setInt(2, rsTokenId.getInt(1));
                     mapInsert.execute();
                 }
-                tokenInsert.execute();
             }
-        } catch (SQLException e) {
+            //preparedStatements
+            mapInsert.close();
+            tokenInsert.close();
+            queryToken.close();
+            queryTokenId.close();
+
+            //resultSets
+            rs.close();
+            rsTokenId.close();
+
+        }
+        catch (SQLException e) {
             e.printStackTrace();
         }
     }

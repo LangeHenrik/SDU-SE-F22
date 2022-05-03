9e2b82c683f7c9adff0a591a0ce15e1e71d36909
Magnus Jensen <magnushjensen.mail@gmail.com>
2022-05-03 11:47:13 +0200

    update migration file, to comply with postgres

--- src/main/resources/dk/sdu/se_f22/sharedlibrary/db/migrations/2.2_token_parameters.sql
+++ src/main/resources/dk/sdu/se_f22/sharedlibrary/db/migrations/2.2_token_parameters.sql
@@ -1,6 +1,6 @@
-CREATE TABLE TokenParameters(
+CREATE TABLE token_parameters(
     id serial PRIMARY KEY,
     delimiter varchar(5) NOT NULL,
-    ignoredChars varchar(64) NOT NULL,
+    ignored_chars varchar(64) NOT NULL,
     type varchar(50) NOT NULL
 );

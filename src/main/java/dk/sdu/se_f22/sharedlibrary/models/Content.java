17e03240c18281a30224693eff13d2c35f6ba639
Mikkel Albrechtsen <mikkel.alb@gmail.com>
2022-05-03 13:43:52 +0200

    Rough content class

--- src/main/java/dk/sdu/se_f22/sharedlibrary/models/Content.java
+++ src/main/java/dk/sdu/se_f22/sharedlibrary/models/Content.java
@@ -1,4 +1,16 @@
 package dk.sdu.se_f22.sharedlibrary.models;
 
 public class Content {
+    private int pageId;
+    private String html;
+    private String title;
+    private String timestamp;
+
+    public Content(int pageId, String html, String title, String timestamp) {
+
+    }
+
+    public Content(int pageId, Document html, String timestamp) {
+
+    }
 }

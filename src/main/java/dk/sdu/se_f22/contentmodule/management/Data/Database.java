c3b766198bbab097cab416558a6b68f8538a6f61
tadayo <daniel.reffeldt@outlook.com>
2022-05-10 16:59:26 +0200

    updated database class to use the pooled database

--- src/main/java/dk/sdu/se_f22/contentmodule/management/Data/Database.java
+++ src/main/java/dk/sdu/se_f22/contentmodule/management/Data/Database.java
@@ -7,8 +7,9 @@ public class Database {
     static protected Database database = null;
 
     private Database() throws SQLException {
-        DriverManager.registerDriver(new org.postgresql.Driver());
-        connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgres","d101201");
+        //DriverManager.registerDriver(new org.postgresql.Driver());
+        //connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgres","password");
+        connection = dk.sdu.se_f22.sharedlibrary.db.DBConnection.getPooledConnection();
     }
 
     protected ResultSet Execute(String str) throws SQLException {

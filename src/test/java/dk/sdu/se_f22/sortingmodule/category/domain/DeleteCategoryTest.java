f9157e739404fe50c59af24be48554cdce3562d1
Suhaib Aslam <suhaib_aslam@hotmail.com>
2022-05-10 10:06:00 +0200

    testDeleteMethod

--- src/test/java/dk/sdu/se_f22/sortingmodule/category/domain/DeleteCategoryTest.java
+++ src/test/java/dk/sdu/se_f22/sortingmodule/category/domain/DeleteCategoryTest.java
@@ -1,5 +1,6 @@
 package dk.sdu.se_f22.sortingmodule.category.domain;
 
+import dk.sdu.se_f22.sortingmodule.category.Category;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
@@ -9,15 +10,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class DeleteCategoryTest {
 
-
     @Test
     void testDeleteCategoryTest() {
         //CategoryDBConnection deleteMethod = new CategoryDBConnection();
-        CategoryDBConnection.shared.createCategory("PC", "Description", "RValue",5);
+        CategoryDBConnection.shared.createCategory("PC", "Description", "RValue",1);
+        CategoryDBConnection.shared.createCategory("PC", "Description1", "RValue1",2);
+
+        Category cs = CategoryDBConnection.shared.getCategoryById(1);
+        System.out.println(cs);
         int del = CategoryDBConnection.shared.deleteCategory(1);
-        //assertEquals(5,CategoryDBConnection.shared.deleteCategory(1));
+        assertEquals(1,CategoryDBConnection.shared.deleteCategory(1));
         assertEquals(1, del);
-
-
     }
 }

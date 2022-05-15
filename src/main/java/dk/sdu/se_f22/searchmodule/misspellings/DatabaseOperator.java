637474e52c6d7bf89d5e25e91b4569dd58567885
OFHansen <ohans21@student.sdu.dk>
2022-05-16 01:55:43 +0200

    Fixed interface

--- src/main/java/dk/sdu/se_f22/searchmodule/misspellings/DatabaseOperator.java
+++ src/main/java/dk/sdu/se_f22/searchmodule/misspellings/DatabaseOperator.java
@@ -1,15 +1,13 @@
 package dk.sdu.se_f22.searchmodule.misspellings;
 
+import dk.sdu.se_f22.searchmodule.infrastructure.interfaces.Filterable;
+
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-public interface DatabaseOperator extends Filterable{
+public interface DatabaseOperator extends Filterable {
     public boolean addMisspelling(String wrong, String correct); //parameter input
     public boolean deleteMisspelling(String missspelling); //parameter input
     public boolean updateMisspelling(String oldMisspelling, String newMisspelling); //parameter input
 }
 
-//Temporary
-interface Filterable {
-    ArrayList<String> filter(ArrayList<String> tokens);
-}

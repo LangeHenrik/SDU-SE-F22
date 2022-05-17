fde478c0266053433f02d9981b4c630617344376
Oscar J. C. Johansen <44866812+OJCJ@users.noreply.github.com>
2022-05-17 10:44:26 +0200

    Fixed spelling mistakes in comments

--- src/main/java/dk/sdu/se_f22/sortingmodule/infrastructure/presentation/SortingModuleDemo.java
+++ src/main/java/dk/sdu/se_f22/sortingmodule/infrastructure/presentation/SortingModuleDemo.java
@@ -7,7 +7,7 @@ public class SortingModuleDemo {
     public static void main (String[] args) {
         SortingModuleImpl module = new SortingModuleImpl();
 
-        // "search" metode, before using below:
+        // "search" method, before using below:
         module.setSearchString("Hello, World!");
         module.addCategory(1);
         module.addCategory(3);
@@ -16,7 +16,7 @@ public class SortingModuleDemo {
         module.setPagination(0, 5);
         SearchHits data = module.search();
 
-        // Data recieved
+        // Data received
         System.out.println("Products: " + data.getProducts().toString());
         System.out.println("Brands: " + data.getBrands().toString());
         System.out.println("Content: " + data.getContents().toString());

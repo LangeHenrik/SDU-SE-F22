b97b6c36be69a97f0426bf18e4fc432c7b6002ef
impostermp <math-351@hotmail.com>
2022-05-22 02:45:26 +0200

    Update ProductIndex.java
    Storing searched products, as well as retrieving these products in descending order(new searches shown first)

--- src/main/java/dk/sdu/se_f22/productmodule/index/ProductIndex.java
+++ src/main/java/dk/sdu/se_f22/productmodule/index/ProductIndex.java
@@ -160,35 +160,78 @@ public class ProductIndex implements IProductIndex, IProductIndexDataAccess {
         }
 
     }
-
-
-    public void createProduct(Product product){
-        // calling stored procedure within db
+    
+    // method that creates and stores products in DB with timestamp of search.
+    public void createSearchProduct(List<Product> searchedProducts){
         try
-                (Connection connection = DBConnection.getPooledConnection();
-                        CallableStatement cs = connection.prepareCall("CALL insertproduct(?,?,?,?,?,?,?,?,?,?,?,?,?)");)
+            (Connection connection = DBConnection.getPooledConnection();
+            )
         {
+            // insert all searched products with timestamp created in a table.
+        for (Product p : searchedProducts) {
+            PreparedStatement searchStatement = connection.prepareStatement("INSERT INTO searches VALUES(?,?)");
+            searchStatement.setString(1,p.getUuid().toString());
+            searchStatement.setTimestamp(2,new Timestamp(System.currentTimeMillis()));
+        }
+        }
 
-            cs.setString(1,product.getUuid().toString());
-            cs.setDouble(2,product.getAverageUserReview());
-            cs.setArray(3,connection.createArrayOf("VARCHAR",product.getInStock().toArray()));
-            cs.setLong(4,product.getEan());
-            cs.setDouble(5,product.getPrice());
-            cs.setTimestamp(6,Timestamp.from(product.getPublishedDate()));
-            cs.setTimestamp(7,Timestamp.from(product.getExpirationDate()));
-            cs.setString(8,product.getCategory());
-            cs.setString(9,product.getName());
-            cs.setString(10,product.getDescription());
-            cs.setString(11,product.getSize());
-            cs.setDouble(12,product.getClockspeed());
-            cs.setDouble(13,product.getWeight());
-
-            cs.execute();
-        } catch (SQLException e) {
+        catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
+    // method that gets all products searched for in a descending order of the creationdate. Showing all newly
+    // searches first.
+    public List<Product> getProductSearched(){
+        List<Product> productList = new ArrayList<>();
+
+        try
+                (Connection connection = DBConnection.getPooledConnection();)
+        {
+            // query all products
+
+            PreparedStatement p = connection.prepareStatement("select product.productid,averageuserreview,ean," +
+                    "price,publisheddate,expirationdate,categories.category,name,description,size,clockspeed,weight," +
+                    "searches.creationdate from searches\n" +
+                    "LEFT JOIN product on product.productid = searches.productid\n" +
+                    "LEFT JOIN  productspecs on product.productid = productspecs.productid\n" +
+                    "LEFT JOIN  specs on productspecs.specid = specs.specid\n" +
+                    "LEFT JOIN  productstorage on productstorage.productid = product.productid\n" +
+                    "LEFT JOIN  storage on productstorage.storageid = storage.storageid\n" +
+                    "LEFT JOIN categories on categories.categoryid = product.categoryid\n" +
+                    "ORDER BY searches.creationdate DESC;");
+            ResultSet resultSet = p.executeQuery();
+
+            while(resultSet.next()){
+                List<String> citylist = new ArrayList<>();
+
+                //iterate through cities in the city column of stock table for all products
+                PreparedStatement pcity = connection.prepareStatement("select city from stock" +
+                        " inner join productstock on productstock.stockid = stock.stockid " +
+                        "INNER JOIN product on product.productid = productstock.productid" +
+                        " WHERE product.productid = ?");
+                pcity.setString(1,resultSet.getString(1));
+                ResultSet citySet = pcity.executeQuery();
+
+                while(citySet.next()){
+                    citylist.add(citySet.getString(1));
+                }
+
+                // create product object
+                productList.add(new Product(UUID.fromString(resultSet.getString(1)),
+                        resultSet.getDouble(2), citylist,resultSet.getInt(3),
+                        resultSet.getDouble(4),resultSet.getTimestamp(5).toInstant(),
+                        resultSet.getTimestamp(6).toInstant(), resultSet.getString(7),
+                        resultSet.getString(8), resultSet.getString(9),
+                        resultSet.getString(10), resultSet.getDouble(11),
+                        resultSet.getDouble(12)
+                ));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return productList;
+    }
 
     public List<Product> getProducts(){
         List<Product> productList = new ArrayList<>();
@@ -232,13 +275,41 @@ public class ProductIndex implements IProductIndex, IProductIndexDataAccess {
                         resultSet.getString(10), resultSet.getDouble(11),
                         resultSet.getDouble(12)
                 ));
-
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return productList;
     }
+
+    public void createProduct(Product product){
+        // calling stored procedure within db
+        try
+                (Connection connection = DBConnection.getPooledConnection();
+                        CallableStatement cs = connection.prepareCall("CALL insertproduct(?,?,?,?,?,?,?,?,?,?,?,?,?)");)
+        {
+
+            cs.setString(1,product.getUuid().toString());
+            cs.setDouble(2,product.getAverageUserReview());
+            cs.setArray(3,connection.createArrayOf("VARCHAR",product.getInStock().toArray()));
+            cs.setLong(4,product.getEan());
+            cs.setDouble(5,product.getPrice());
+            cs.setTimestamp(6,Timestamp.from(product.getPublishedDate()));
+            cs.setTimestamp(7,Timestamp.from(product.getExpirationDate()));
+            cs.setString(8,product.getCategory());
+            cs.setString(9,product.getName());
+            cs.setString(10,product.getDescription());
+            cs.setString(11,product.getSize());
+            cs.setDouble(12,product.getClockspeed());
+            cs.setDouble(13,product.getWeight());
+
+            cs.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
 
 

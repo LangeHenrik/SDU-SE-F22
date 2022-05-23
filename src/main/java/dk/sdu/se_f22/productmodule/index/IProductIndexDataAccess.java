c9c7fc09c62f7171087ba560e48bebc1225a6aac
Nikolaj <nstau21@student.sdu.dk>
2022-05-23 21:07:13 +0200

    Fixed update method

--- src/main/java/dk/sdu/se_f22/productmodule/index/IProductIndexDataAccess.java
+++ src/main/java/dk/sdu/se_f22/productmodule/index/IProductIndexDataAccess.java
@@ -3,7 +3,7 @@ package dk.sdu.se_f22.productmodule.index;
 import dk.sdu.se_f22.sharedlibrary.models.Product;
 
 public interface IProductIndexDataAccess {
-    public void updateProduct(int id, Product product);
+    public void updateProduct(int id, Product product, int productID, int specID, int storageID);
     public void deleteProduct(int id);
     public void createProduct(Product product);
 }

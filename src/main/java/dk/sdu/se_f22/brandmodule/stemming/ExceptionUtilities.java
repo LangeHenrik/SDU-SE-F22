ae3d4bbaec0146a70629a0f335d5f87b18f4eedf
Magnus Christian Larsen <winto@LapMag.localdomain>
2022-05-24 12:14:31 +0200

    Swapped format strings in favor of prepared statements

--- src/main/java/dk/sdu/se_f22/brandmodule/stemming/ExceptionUtilities.java
+++ src/main/java/dk/sdu/se_f22/brandmodule/stemming/ExceptionUtilities.java
@@ -2,37 +2,53 @@ package dk.sdu.se_f22.brandmodule.stemming;
 
 import dk.sdu.se_f22.sharedlibrary.db.DBConnection;
 
+import javax.xml.transform.Result;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
 
 public class ExceptionUtilities {
     public static void createException(String name) throws SQLException {
-        String query = String.format("INSERT INTO stemmingexceptions (exception) VALUES ('%s')", name);
-        DBConnection.getPooledConnection().createStatement().executeUpdate(query);
+        Connection connection = DBConnection.getPooledConnection();
+        PreparedStatement statement = connection.prepareStatement("INSERT INTO stemmingexceptions (exception) VALUES (?)");
+        statement.setString(1, name);
+        statement.executeUpdate();
+        connection.close();
     }
 
     public static HashMap<Integer, String> getExceptions() throws SQLException {
+        Connection connection = DBConnection.getPooledConnection();
         String query = "SELECT * FROM stemmingexceptions";
-        ResultSet result = DBConnection.getPooledConnection().createStatement().executeQuery(query);
+        ResultSet result = connection.prepareStatement(query).executeQuery();
         return handleResult(result);
     }
 
     public static HashMap<Integer, String> getException(String name) throws SQLException {
-        String query = String.format("SELECT * FROM stemmingexceptions WHERE exception = '%s'", name);
-        ResultSet result = DBConnection.getPooledConnection().createStatement().executeQuery(query);
+        Connection connection = DBConnection.getPooledConnection();
+        PreparedStatement statement = connection.prepareStatement("SELECT * FROM stemmingexceptions WHERE exception = ?");
+        statement.setString(1, name);
+        ResultSet result = statement.executeQuery();
         return handleResult(result);
 
     }
 
     public static void updateException(String name, String newName) throws SQLException {
-        String query = String.format("UPDATE stemmingexceptions SET exception = '%s' WHERE exception = '%s'", newName, name);
-        DBConnection.getPooledConnection().createStatement().executeUpdate(query);
+        Connection connection = DBConnection.getPooledConnection();
+        PreparedStatement statement = connection.prepareStatement("UPDATE stemmingexceptions SET exception = ?" + "WHERE exception = ?");
+        statement.setString(1, newName);
+        statement.setString(2, name);
+        statement.executeUpdate();
+        connection.close();
     }
 
     public static void deleteException(String name) throws SQLException {
-        String query = String.format("DELETE FROM stemmingexceptions WHERE exception = '%s'", name);
-        DBConnection.getPooledConnection().createStatement().executeUpdate(query);
+        Connection connection = DBConnection.getPooledConnection();
+        PreparedStatement statement = connection.prepareStatement("DELETE FROM stemmingexceptions " + "WHERE exception =  ?");
+        statement.setString(1, name);
+        statement.executeUpdate();
+        connection.close();
     }
 
     private static HashMap<Integer, String> handleResult(ResultSet result) throws SQLException {

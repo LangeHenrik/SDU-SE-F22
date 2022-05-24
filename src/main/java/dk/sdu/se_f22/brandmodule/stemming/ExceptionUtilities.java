507c7417f364e2e16a703a7acbb0621d96072a71
Magnus Christian Larsen <winto@LapMag.localdomain>
2022-05-24 11:27:18 +0200

    Finished full implementation of database exceptions

--- src/main/java/dk/sdu/se_f22/brandmodule/stemming/ExceptionUtilities.java
+++ src/main/java/dk/sdu/se_f22/brandmodule/stemming/ExceptionUtilities.java
@@ -8,31 +8,38 @@ import java.util.HashMap;
 
 public class ExceptionUtilities {
     public static void createException(String name) throws SQLException {
-        String query = String.format("INSERT INTO StemmingException (name) VALUES ('%s')", name);
-        ResultSet result = DBConnection.getConnection().createStatement().executeQuery(query);
-        result.close();
+        String query = String.format("INSERT INTO stemmingexceptions (exception) VALUES ('%s')", name);
+        DBConnection.getPooledConnection().createStatement().executeUpdate(query);
     }
     public static HashMap<Integer, String> getExceptions() throws SQLException {
-        String query = String.format("SELECT * FROM StemmingException");
-        ResultSet result = DBConnection.getConnection().createStatement().executeQuery(query);
-        result.close();
+        String query = String.format("SELECT * FROM stemmingexceptions");
+        ResultSet result = DBConnection.getPooledConnection().createStatement().executeQuery(query);
+        return handleResult(result);
+    }
+    public static HashMap<Integer, String> getException(String name) throws SQLException {
+        String query = String.format("SELECT * FROM stemmingexceptions WHERE exception = '%s'", name);
+        ResultSet result = DBConnection.getPooledConnection().createStatement().executeQuery(query);
         return handleResult(result);
 
     }
-    public static HashMap<Integer, String> findException(String name) throws SQLException {
-        String query = String.format("SELECT * FROM StemmingException WHERE name = '%s';", name);
-        ResultSet result = DBConnection.getConnection().createStatement().executeQuery(query);
-        result.close();
-        return handleResult(result);
+    public static void updateException(String name, String newName) throws SQLException {
+        String query = String.format("UPDATE stemmingexceptions SET exception = '%s' WHERE exception = '%s'", newName, name);
+        DBConnection.getPooledConnection().createStatement().executeUpdate(query);
     }
+    public static void deleteException(String name) throws SQLException {
+        String query = String.format("DELETE FROM stemmingexceptions WHERE exception = '%s'", name);
+        DBConnection.getPooledConnection().createStatement().executeUpdate(query);
+    }
+
 
     private static HashMap<Integer, String> handleResult(ResultSet result) throws SQLException {
         HashMap<Integer, String> map = new HashMap<>();
         while(result.next()) {
             int id = result.getInt("id");
-            String name = result.getString("name");
+            String name = result.getString("exception");
             map.put(id, name);
         }
+        result.close();
         return map;
     }
 }
--- src/main/java/dk/sdu/se_f22/brandmodule/stemming/IStemmer.java
+++ src/main/java/dk/sdu/se_f22/brandmodule/stemming/IStemmer.java
@@ -1,10 +1,16 @@
 package dk.sdu.se_f22.brandmodule.stemming;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public interface IStemmer {
   public String stem(String word);
   
   public ArrayList<String> stem(List<String> word);
+  public void addException(String exception);
+  public List<Collection<String>> getExceptions();
+  public void updateException(String exception, String newException);
+  public void removeException(String exception);
+  public boolean exceptionExists(String exception);
 }

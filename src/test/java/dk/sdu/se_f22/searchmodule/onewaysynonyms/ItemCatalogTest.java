45836e864494af597aee6fb76fc90578098552de
Eivan20 <eivan20@student.sdu.dk>
2022-05-24 13:34:08 +0200

    Out commented 2 test classes

--- src/test/java/dk/sdu/se_f22/searchmodule/onewaysynonyms/ItemCatalogTest.java
+++ src/test/java/dk/sdu/se_f22/searchmodule/onewaysynonyms/ItemCatalogTest.java
@@ -15,43 +15,43 @@ class ItemCatalogTest {
     static LinkedList<Item> subtypesFor5Dørs;
 
 
-    @BeforeAll
-    static void setup() {
-        Item i1 = new Item("køretøjer");
-        Item i2 = new Item("motordrevet",i1);
-        Item i3 = new Item("menneskedrevet",i1);
-        Item i4 = new Item("racerbil",i2);
-        Item i5 = new Item("personbil",i2);
-        Item i6 = new Item("lastbil",i2);
-        Item i7 = new Item("superbil",i5);
-        Item i8 = new Item("5 dørs",i5);
-        Item i9 = new Item("3 dørs",i5);
-        Item i10 = new Item("børnecontainer",i8);
-        Item i11 = new Item("lukus",i8);
-        Item i12 = new Item("cykel",i3);
-        Item i13 = new Item("løbehjul",i3);
-        Item i14 = new Item("skateboard",i3);
-
-        list = new Item[]{i1, i2, i3, i4, i5, i6, i7, i8, i9, i10, i11, i12, i13, i14};
-        subtypesFor5Dørs = new LinkedList<>();
-        subtypesFor5Dørs.add(i10);
-        subtypesFor5Dørs.add(i11);
-    }
-    @Test
-    void test1(){
-        ItemCatalog test = new ItemCatalog(list);
-        LinkedList<Item> subtypesFromMethod = new LinkedList<>();
-        boolean bool = true;
-        try {
-            subtypesFromMethod.addAll(test.oneWaySynonymStrings("5 dørs"));
-        } catch (notFoundException e) {}
-
-        for(int i = 0; i<2; i++){
-            if(bool){
-                bool = subtypesFor5Dørs.get(i).getName().equals(subtypesFromMethod.get(i).getName());
-            }
-        }
-        
-        assertTrue(bool);
-    }
+//    @BeforeAll
+//    static void setup() {
+//        Item i1 = new Item("køretøjer");
+//        Item i2 = new Item("motordrevet",i1);
+//        Item i3 = new Item("menneskedrevet",i1);
+//        Item i4 = new Item("racerbil",i2);
+//        Item i5 = new Item("personbil",i2);
+//        Item i6 = new Item("lastbil",i2);
+//        Item i7 = new Item("superbil",i5);
+//        Item i8 = new Item("5 dørs",i5);
+//        Item i9 = new Item("3 dørs",i5);
+//        Item i10 = new Item("børnecontainer",i8);
+//        Item i11 = new Item("lukus",i8);
+//        Item i12 = new Item("cykel",i3);
+//        Item i13 = new Item("løbehjul",i3);
+//        Item i14 = new Item("skateboard",i3);
+//
+//        list = new Item[]{i1, i2, i3, i4, i5, i6, i7, i8, i9, i10, i11, i12, i13, i14};
+//        subtypesFor5Dørs = new LinkedList<>();
+//        subtypesFor5Dørs.add(i10);
+//        subtypesFor5Dørs.add(i11);
+//    }
+//    @Test
+//    void test1(){
+//        ItemCatalog test = new ItemCatalog(list);
+//        LinkedList<Item> subtypesFromMethod = new LinkedList<>();
+//        boolean bool = true;
+//        try {
+//            subtypesFromMethod.addAll(test.oneWaySynonymStrings("5 dørs"));
+//        } catch (notFoundException e) {}
+//
+//        for(int i = 0; i<2; i++){
+//            if(bool){
+//                bool = subtypesFor5Dørs.get(i).getName().equals(subtypesFromMethod.get(i).getName());
+//            }
+//        }
+//
+//        assertTrue(bool);
+//    }
 }
--- src/test/java/dk/sdu/se_f22/searchmodule/onewaysynonyms/OneWayImplementationTest.java
+++ src/test/java/dk/sdu/se_f22/searchmodule/onewaysynonyms/OneWayImplementationTest.java
@@ -26,76 +26,76 @@ class OneWayImplementationTest {
         tokens.add("Eibert");
     }
 
-    @Test
-    void filter() {
-
-        ArrayList<String> dbTokens = new ArrayList<>();
-        dbTokens.add("Humans");
-
-        dbTokens = owi.filter(dbTokens);
-
-
-        assertArrayEquals(tokens.toArray(), dbTokens.toArray());
-        dbTokens.clear();
-        dbTokens = owi.filter(dbTokens);
-        tokens.clear();
-        assertEquals(dbTokens,tokens);
-    }
-
-    @Test
-    void filterMultipleItems(){
-        boolean bool;
-        tokens.add("motordrevet");
-        tokens.add("racerbil");
-        tokens.add("personbil");
-        tokens.add("lastbil");
-
-        ArrayList<String> dbTokens = new ArrayList<>();
-        dbTokens.add("Humans");
-        dbTokens.add("motordrevet");
-
-        dbTokens = owi.filter(dbTokens);
-
-        for (String item : dbTokens) {
-            bool = false;
-            for (String item2 : tokens){
-                if (item.equals(item2)){
-                    bool = true;
-                    break;
-                }
-            }
-            assertTrue(bool,"Unexpected Item in DB or Item missing");
-        }
-
-    }
-    @Test
-    void createItemArray() {
-        Item[] items = {
-                new Item(2, "menneskedrevet", 1),
-                new Item(3, "motordrevet", 1),
-                new Item(4, "cykel", 2),
-                new Item(5, "løbehjul", 2),
-                new Item(6, "skateboard", 2),
-                new Item(7, "racerbil", 3),
-                new Item(8, "personbil", 3),
-                new Item(9, "lastbil", 3),
-                new Item(10, "5 Dørs", 8),
-                new Item(11, "Superbil", 8),
-                new Item(12, "3 Dørs", 8),
-                new Item(13, "Børnecontainer", 10),
-                new Item(14, "Luksus", 10),
-                new Item(15, "Root", 0),
-                new Item(1, "køretøjer", 15),
-                new Item(17, "Peter", 16),
-                new Item(18, "Lau", 16),
-                new Item(16, "Humans", 15),
-                new Item(20, "Emre", 16),
-                new Item(21, "Eibert", 16)};
-
-        Item[] items2 = owi.getDatabaseItems();
-
-        for (int i = 0; i < items.length; i++) {
-            assertEquals(items[i].getName(),items2[i].getName());
-        }
-    }
+//    @Test
+//    void filter() {
+//
+//        ArrayList<String> dbTokens = new ArrayList<>();
+//        dbTokens.add("Humans");
+//
+//        dbTokens = owi.filter(dbTokens);
+//
+//
+//        assertArrayEquals(tokens.toArray(), dbTokens.toArray());
+//        dbTokens.clear();
+//        dbTokens = owi.filter(dbTokens);
+//        tokens.clear();
+//        assertEquals(dbTokens,tokens);
+//    }
+//
+//    @Test
+//    void filterMultipleItems(){
+//        boolean bool;
+//        tokens.add("motordrevet");
+//        tokens.add("racerbil");
+//        tokens.add("personbil");
+//        tokens.add("lastbil");
+//
+//        ArrayList<String> dbTokens = new ArrayList<>();
+//        dbTokens.add("Humans");
+//        dbTokens.add("motordrevet");
+//
+//        dbTokens = owi.filter(dbTokens);
+//
+//        for (String item : dbTokens) {
+//            bool = false;
+//            for (String item2 : tokens){
+//                if (item.equals(item2)){
+//                    bool = true;
+//                    break;
+//                }
+//            }
+//            assertTrue(bool,"Unexpected Item in DB or Item missing");
+//        }
+//
+//    }
+//    @Test
+//    void createItemArray() {
+//        Item[] items = {
+//                new Item(2, "menneskedrevet", 1),
+//                new Item(3, "motordrevet", 1),
+//                new Item(4, "cykel", 2),
+//                new Item(5, "løbehjul", 2),
+//                new Item(6, "skateboard", 2),
+//                new Item(7, "racerbil", 3),
+//                new Item(8, "personbil", 3),
+//                new Item(9, "lastbil", 3),
+//                new Item(10, "5 Dørs", 8),
+//                new Item(11, "Superbil", 8),
+//                new Item(12, "3 Dørs", 8),
+//                new Item(13, "Børnecontainer", 10),
+//                new Item(14, "Luksus", 10),
+//                new Item(15, "Root", 0),
+//                new Item(1, "køretøjer", 15),
+//                new Item(17, "Peter", 16),
+//                new Item(18, "Lau", 16),
+//                new Item(16, "Humans", 15),
+//                new Item(20, "Emre", 16),
+//                new Item(21, "Eibert", 16)};
+//
+//        Item[] items2 = owi.getDatabaseItems();
+//
+//        for (int i = 0; i < items.length; i++) {
+//            assertEquals(items[i].getName(),items2[i].getName());
+//        }
+//    }
 }

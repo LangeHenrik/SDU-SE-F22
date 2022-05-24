9e5978c54284d5e5e630a59f2c862d74832c59a1
Patrick <Patrickbjerre@outlook.dk>
2022-05-24 13:09:49 +0200

    Reverted Main

--- src/main/java/dk/sdu/se_f22/Main.java
+++ src/main/java/dk/sdu/se_f22/Main.java
@@ -1,15 +1,11 @@
 package dk.sdu.se_f22;
 
-import dk.sdu.se_f22.brandmodule.management.Bim;
 import dk.sdu.se_f22.brandmodule.management.services.IndexingService;
-import dk.sdu.se_f22.sharedlibrary.db.DBMigration;
-import dk.sdu.se_f22.sharedlibrary.db.MigrationException;
 
 import java.security.Provider;
 
 
 public class Main {
     public static void main(String[] args) {
-
     }
 }

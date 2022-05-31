9db18e31649702ddb6ef155c5430cb436254c9c7
schwartz21 <pschw21@student.sdu.dk>
2022-05-31 21:11:01 +0200

    Updated demo

--- src/main/java/dk/sdu/se_f22/sortingmodule/category/domain/CategoryDemo.java
+++ src/main/java/dk/sdu/se_f22/sortingmodule/category/domain/CategoryDemo.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-public class Main {
+public class CategoryDemo {
 
     public static void main(String[] args) {
         CategoryCRUDInterface categoryCRUD = new CategoryCRUD();
